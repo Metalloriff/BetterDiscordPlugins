010a7f137dfab43d455859dcceb7c29ee3673871
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-03-09 00:45:59 -0500

    Update AvatarIconViewer.plugin.js
    You can now view svg images and group DM avatars. Images now download a lot faster.

--- AvatarIconViewer.plugin.js
+++ AvatarIconViewer.plugin.js
@@ -4,7 +4,7 @@ class AvatarIconViewer {
 	
     getName() { return "User Avatar And Server Icon Viewer"; }
     getDescription() { return "Allows you to view server icons and user avatars in fullscreen by right clicking on a user's avatar or a server icon. You can also directly copy the image URL or open the URL externally. REQUIRES DARK THEME! If you need any help or have any suggestions, feel free to message me, Metalloriff#2891."; }
-    getVersion() { return "0.1.1"; }
+    getVersion() { return "0.1.2"; }
     getAuthor() { return "Metalloriff"; }
 
     load() {}
@@ -32,13 +32,17 @@ class AvatarIconViewer {
 		if(AvatarIconViewer.clickedTooSoon == false){
 			var x = event.clientX, y = event.clientY, elementMouseIsOver = document.elementFromPoint(x, y), context = document.getElementsByClassName("contextMenu-uoJTbz theme-dark")[0];
 			if(context){
-				if(context.outerHTML.includes("Mark As Read")){
+				if(elementMouseIsOver.parentNode.outerHTML.includes("guild-inner")){
 					AvatarIconViewer.url = AvatarIconViewer.getBetween(elementMouseIsOver.outerHTML, "url(&quot;", "&quot;)").replace(".webp", ".png") + "?size=2048";
 					context.firstChild.insertAdjacentHTML("beforeend", "<div class=\"item-1XYaYf\" onclick=\"AvatarIconViewer.createImagePreview();\"><span>View Icon</span><div class=\"hint-3TJykr\"></div></div>");
 					context.firstChild.insertAdjacentHTML("beforeend", "<div class=\"item-1XYaYf\" onclick=\"AvatarIconViewer.copyURL();\"><span>Copy Icon URL</span><div class=\"hint-3TJykr\"></div></div>");
 				}
-				if(context.outerHTML.includes("Profile") && elementMouseIsOver.outerHTML.includes("avatar")){	
-					AvatarIconViewer.url = AvatarIconViewer.getBetween(elementMouseIsOver.outerHTML, "url(&quot;", "?size").replace(".webp", ".png") + "?size=2048";
+				if(elementMouseIsOver.outerHTML.includes("avatar")){
+					AvatarIconViewer.url = AvatarIconViewer.getBetween(elementMouseIsOver.outerHTML, "url(&quot;", "&quot;)");
+					if(AvatarIconViewer.url.includes("?"))
+						AvatarIconViewer.url = AvatarIconViewer.url.substr(0, AvatarIconViewer.url.indexOf("?"));
+					AvatarIconViewer.url += "?size=2048";
+					console.log(AvatarIconViewer.url);
 					context.firstChild.insertAdjacentHTML("beforeend", "<div class=\"item-1XYaYf\" onclick=\"AvatarIconViewer.createImagePreview();\"><span>View Avatar</span><div class=\"hint-3TJykr\"></div></div>");
 					context.firstChild.insertAdjacentHTML("beforeend", "<div class=\"item-1XYaYf\" onclick=\"AvatarIconViewer.copyURL();\"><span>Copy Avatar URL</span><div class=\"hint-3TJykr\"></div></div>");
 				}

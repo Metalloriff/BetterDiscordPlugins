b27d6f97b6734877876575290d40ccd469c53afc
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-03-31 17:23:13 -0400

    v0.3.7

--- AvatarIconViewer.plugin.js
+++ AvatarIconViewer.plugin.js
@@ -7,16 +7,9 @@ class AvatarIconViewer {
 		this.url = "";
 	}
 	
-	get themeType(){
-		if(!$(".theme-dark").length)
-			return "light";
-		else
-			return "dark";
-	}
-	
     getName() { return "User Avatar And Server Icon Viewer"; }
     getDescription() { return "Allows you to view server icons, user avatars, and emotes in fullscreen via the context menu. You may also directly copy the image URL or open the URL externally."; }
-    getVersion() { return "0.3.6"; }
+    getVersion() { return "0.3.7"; }
     getAuthor() { return "Metalloriff"; }
 
     load() {}
@@ -40,11 +33,11 @@ class AvatarIconViewer {
 		$(document).on("contextmenu.AvatarIconViewer", e => { this.onContextMenu(e); });
 	}
 	
-	onContextMenu() {
+	onContextMenu(e) {
 		if(this.clickedTooSoon == false){
-			var x = event.clientX, y = event.clientY, target = event.target, context = $(".contextMenu-uoJTbz")[0], messageGroup = null, userData = null, memberElement, createContext = false, viewLabel, copyLabel;
+			var target = e.target, context = $(".contextMenu-uoJTbz")[0], messageGroup = null, userData = null, memberElement, createContext = false, viewLabel, copyLabel;
 			if(context){
-				$(".member.member-status").toArray().forEach(function(e){
+				$(".member-2FrNV0").toArray().forEach(function(e){
 					if(e.outerHTML.includes(target.outerHTML))
 						memberElement = e;
 				});
@@ -63,7 +56,7 @@ class AvatarIconViewer {
 					});
 				}
 				if(messageGroup != null){
-					var messages = messageGroup[Object.keys(messageGroup).find((key) => key.startsWith('__reactInternalInstance'))].return.memoizedProps.messages;
+					var messages = ReactUtilities.getReactInstance(messageGroup).return.memoizedProps.messages;
 					if(messages != null && messages.length > 0)
 						userData = messages[0].author;
 				}
@@ -112,8 +105,8 @@ class AvatarIconViewer {
 		if(document.getElementById("avatar-img-preview") == null){
 			$(document).on("keyup.AvatarIconViewer", e => { this.onEscape(e); });
 			this.closeContextMenu();
-			var p = $(".theme-" + this.themeType).last(), scale = window.innerHeight - 160;
-			p.append(`<div id="aiv-preview-window" class=""><div id="aiv-preview-backdrop" class="backdrop-2ohBEd" style="opacity: 0.85; background-color: rgb(0, 0, 0); transform: translateZ(0px);"></div><div class="modal-2LIEKY" style="opacity: 1; transform: scale(1) translateZ(0px);"><div class="inner-1_1f7b"><div><div class="imageWrapper-38T7d9" style="width: ` + scale + `px; height: ` + scale + `px;"><img src="` + this.url + `" style="width: 100%; height: 100%;"></div><div style="text-align: center; padding-top: 5px;"><button id="aiv-preview-copy" class="button-2t3of8 lookFilled-luDKDo colorBrand-3PmwCE sizeMedium-2VGNaF grow-25YQ8u" type="button" style="display: inline-block; height: 30px !important; min-height: 30px !important; margin-right: 5px; margin-left: 5px;"><div class="contents-4L4hQM">Copy URL</div></button><button id="aiv-preview-close" class="button-2t3of8 lookFilled-luDKDo colorBrand-3PmwCE sizeMedium-2VGNaF grow-25YQ8u" type="button" style="display: inline-block; height: 30px !important; min-height: 30px !important; margin-right: 5px; margin-left: 5px;"><div class="contents-4L4hQM">Close</div></button><button id="aiv-preview-open" class="button-2t3of8 lookFilled-luDKDo colorBrand-3PmwCE sizeMedium-2VGNaF grow-25YQ8u" type="button" style="display: inline-block; height: 30px !important; min-height: 30px !important; margin-right: 5px; margin-left: 5px;"><div class="contents-4L4hQM">Open Externally</div></button></div></div></div></div></div>`);
+			var app = $(".app").last(), scale = window.innerHeight - 160;
+			app.append(`<div id="aiv-preview-window" class=""><div id="aiv-preview-backdrop" class="backdrop-2ohBEd" style="opacity: 0.85; background-color: rgb(0, 0, 0); transform: translateZ(0px);"></div><div class="modal-2LIEKY" style="opacity: 1; transform: scale(1) translateZ(0px);"><div class="inner-1_1f7b"><div><div class="imageWrapper-38T7d9" style="width: ` + scale + `px; height: ` + scale + `px;"><img src="` + this.url + `" style="width: 100%; height: 100%;"></div><div style="text-align: center; padding-top: 5px;"><button id="aiv-preview-copy" class="button-2t3of8 lookFilled-luDKDo colorBrand-3PmwCE sizeMedium-2VGNaF grow-25YQ8u" type="button" style="display: inline-block; height: 30px !important; min-height: 30px !important; margin-right: 5px; margin-left: 5px;"><div class="contents-4L4hQM">Copy URL</div></button><button id="aiv-preview-close" class="button-2t3of8 lookFilled-luDKDo colorBrand-3PmwCE sizeMedium-2VGNaF grow-25YQ8u" type="button" style="display: inline-block; height: 30px !important; min-height: 30px !important; margin-right: 5px; margin-left: 5px;"><div class="contents-4L4hQM">Close</div></button><button id="aiv-preview-open" class="button-2t3of8 lookFilled-luDKDo colorBrand-3PmwCE sizeMedium-2VGNaF grow-25YQ8u" type="button" style="display: inline-block; height: 30px !important; min-height: 30px !important; margin-right: 5px; margin-left: 5px;"><div class="contents-4L4hQM">Open Externally</div></button></div></div></div></div></div>`);
 			$("#aiv-preview-backdrop").on("click", e => { this.destroyPreview(e); });
 			$("#aiv-preview-copy").on("click", e => { this.copyURL(e); });
 			$("#aiv-preview-close").on("click", e => { this.destroyPreview(e); });

057c38cbaeee275a4755802a540fe56b23bcaea4
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-04-28 11:06:13 -0400

    v0.1.6

--- GuildAndFriendRemovalAlerts.plugin.js
+++ GuildAndFriendRemovalAlerts.plugin.js
@@ -20,7 +20,7 @@ class GuildAndFriendRemovalAlerts {
 	
     getName() { return "Guild And Friend Removal Alerts"; }
     getDescription() { return "Alerts you when a guild or friend is removed."; }
-    getVersion() { return "0.1.5"; }
+    getVersion() { return "0.1.6"; }
     getAuthor() { return "Metalloriff"; }
 
     load() {}
@@ -37,38 +37,38 @@ class GuildAndFriendRemovalAlerts {
 		if(panel.length){
 			panel.append(`<h style="color: white;font-size: 30px;font-weight: bold;">Guild And Friend Removal Alerts by Metalloriff</h>
 				<div style="padding-top: 20px;">
-				   <div id="ra-settings-checkboxgroup" class="radioGroup-2P3MJo">
-					  <div id="ra-checkbox-1" class="item-2zi_5J marginBottom8-1mABJ4 horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ cardPrimaryEditable-2IQ7-V card-3DrRmC" style="padding: 10px;border-radius: 0px !important;">
-						 <label class="checkboxWrapper-2Yvr_Y">
-							<input type="checkbox" class="inputDefault-2tiBIA input-oWyROL" value="on">
-							<div class="checkbox-1QwaS4 flexCenter-28Hs0n flex-3B1Tl4 justifyCenter-29N31w alignCenter-3VxkQP box-XhjOl4">
+				   <div id="ra-settings-checkboxgroup" class="radioGroup-1GBvlr">
+					  <div id="ra-checkbox-1" class="item-26Dhrx marginBottom8-AtZOdT horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG cardPrimaryEditable-3KtE4g card-3Qj_Yx" style="padding: 10px;border-radius: 0px !important;">
+						 <label class="checkboxWrapper-SkhIWG">
+							<input type="checkbox" class="inputDefault-3JxKJ2 input-3ITkQf" value="on">
+							<div class="checkbox-1ix_J3 flexCenter-3_1bcw flex-1O1GKY justifyCenter-3D2jYp alignCenter-1dQNNs box-mmYMsp">
 							<div id="ra-checkbox-ticked-1" style="background-color: white;height: 60%;width: 60%;border-radius: 3px;"></div>
 							</div>
 						 </label>
-						 <div class="info-1Z508c">
-							<div class="title-1M-Ras">Guild alerts</div>
+						 <div class="info-3LOr12">
+							<div class="title-3BE6m5">Guild alerts</div>
 						 </div>
 					  </div>
-					  <div id="ra-checkbox-2" class="item-2zi_5J marginBottom8-1mABJ4 horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ cardPrimaryEditable-2IQ7-V card-3DrRmC" style="padding: 10px;border-radius: 0px !important;">
-						 <label class="checkboxWrapper-2Yvr_Y">
-							<input type="checkbox" class="inputDefault-2tiBIA input-oWyROL" value="on">
-							<div class="checkbox-1QwaS4 flexCenter-28Hs0n flex-3B1Tl4 justifyCenter-29N31w alignCenter-3VxkQP box-XhjOl4">
+					  <div id="ra-checkbox-2" class="item-26Dhrx marginBottom8-AtZOdT horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG cardPrimaryEditable-3KtE4g card-3Qj_Yx" style="padding: 10px;border-radius: 0px !important;">
+						 <label class="checkboxWrapper-SkhIWG">
+							<input type="checkbox" class="inputDefault-3JxKJ2 input-3ITkQf" value="on">
+							<div class="checkbox-1ix_J3 flexCenter-3_1bcw flex-1O1GKY justifyCenter-3D2jYp alignCenter-1dQNNs box-mmYMsp">
 							<div id="ra-checkbox-ticked-2" style="background-color: white;height: 60%;width: 60%;border-radius: 3px;"></div>
 							</div>
 						 </label>
-						 <div class="info-1Z508c">
-							<div class="title-1M-Ras">Friend alerts</div>
+						 <div class="info-3LOr12">
+							<div class="title-3BE6m5">Friend alerts</div>
 						 </div>
 					  </div>
-					  <div id="ra-checkbox-3" class="item-2zi_5J marginBottom8-1mABJ4 horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ cardPrimaryEditable-2IQ7-V card-3DrRmC" style="padding: 10px;border-radius: 0px !important;">
-						 <label class="checkboxWrapper-2Yvr_Y">
-							<input type="checkbox" class="inputDefault-2tiBIA input-oWyROL" value="on">
-							<div class="checkbox-1QwaS4 flexCenter-28Hs0n flex-3B1Tl4 justifyCenter-29N31w alignCenter-3VxkQP box-XhjOl4">
+					  <div id="ra-checkbox-3" class="item-26Dhrx marginBottom8-AtZOdT horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG cardPrimaryEditable-3KtE4g card-3Qj_Yx" style="padding: 10px;border-radius: 0px !important;">
+						 <label class="checkboxWrapper-SkhIWG">
+							<input type="checkbox" class="inputDefault-3JxKJ2 input-3ITkQf" value="on">
+							<div class="checkbox-1ix_J3 flexCenter-3_1bcw flex-1O1GKY justifyCenter-3D2jYp alignCenter-1dQNNs box-mmYMsp">
 							<div id="ra-checkbox-ticked-3" style="background-color: white;height: 60%;width: 60%;border-radius: 3px;"></div>
 							</div>
 						 </label>
-						 <div class="info-1Z508c">
-							<div class="title-1M-Ras">Show Windows notifications</div>
+						 <div class="info-3LOr12">
+							<div class="title-3BE6m5">Show Windows notifications</div>
 						 </div>
 					  </div>
 				   </div>
@@ -157,22 +157,22 @@ class GuildAndFriendRemovalAlerts {
 			if(!guildIDs.includes(guild.id)){
 				if(!$("#ra-alertwindow").length){
 					app.append(`<div id="ra-alertwindow">
-					   <div class="backdrop-2ohBEd" style="opacity: 0.85; background-color: rgb(0, 0, 0); transform: translateZ(0px);" onclick="$(this).parent().remove();"></div>
-					   <div id="ra-modal" class="modal-2LIEKY" style="opacity: 1; overflow-y: auto; justify-content: flex-start;"></div>
+					   <div class="backdrop-1ocfXc" style="opacity: 0.85; background-color: rgb(0, 0, 0); transform: translateZ(0px);" onclick="$(this).parent().remove();"></div>
+					   <div id="ra-modal" class="modal-1UGdnR" style="opacity: 1; overflow-y: auto; justify-content: flex-start;"></div>
 					</div>`);
 				}
 				$("#ra-modal").append(`
-					<div class="inner-1_1f7b ra-serveritem" style="margin: 20px; min-height: 134px;">
-					<div class="topSectionNormal-2LlRG1">
-						<header class="header-2Lg0Oe flex-3B1Tl4 alignCenter-3VxkQP">
-							<div class="avatar-1BXaQj profile-3z9uol avatar-4g_GO6"><img id="ra-icon" src="/assets/f046e2247d730629309457e902d5c5b3.svg" height="90" width="90"></div>
-							<div class="headerInfo-Gkqcz9">
+					<div class="inner-1JeGVc ra-serveritem" style="margin: 20px; min-height: 134px;">
+					<div class="topSectionNormal-2-vo2m">
+						<header class="header-QKLPzZ flex-1O1GKY alignCenter-1dQNNs">
+							<div class="avatar-16XVId profile-ZOdGIb avatar-3EQepX"><img id="ra-icon" src="/assets/f046e2247d730629309457e902d5c5b3.svg" height="90" width="90"></div>
+							<div class="headerInfo-30uryT">
 								<div class="nameTag-2n-N0D userSelectText-wz4t4g nameTag-26T3kW">
-									<span id="ra-namelabel" class="username username-24t9uh size18-ZM4Qv- weightSemiBold-T8sxWH" style="padding-right: 7px;">Unknown</span>
-									<span class="username username-24t9uh size18-ZM4Qv- weightSemiBold-T8sxWH" style="float: right; cursor: pointer;" onclick="$(this.parentElement.parentElement.parentElement.parentElement.parentElement).remove();">X</span>
+									<span id="ra-namelabel" class="username username-3gJmXY size18-3EXdSj weightSemiBold-NJexzi" style="padding-right: 7px;">Unknown</span>
+									<span class="username username-3gJmXY size18-3EXdSj weightSemiBold-NJexzi" style="float: right; cursor: pointer;" onclick="$(this.parentElement.parentElement.parentElement.parentElement.parentElement).remove();">X</span>
 								</div>
-								<div class="username username-24t9uh size18-ZM4Qv- weightSemiBold-T8sxWH" style="color: rgb(150, 150, 150); padding-top: 20px">Guild no longer present! It is either temporarliy down, you were kicked/banned, or it was deleted.</div>
-								<div id="ra-ownerlabel" class="username username-24t9uh size18-ZM4Qv- weightSemiBold-T8sxWH" style="color: rgb(150, 150, 150); padding-top: 20px">Owner unknown</div>
+								<div class="username username-3gJmXY size18-3EXdSj weightSemiBold-NJexzi" style="color: rgb(150, 150, 150); padding-top: 20px">Guild no longer present! It is either temporarliy down, you were kicked/banned, or it was deleted.</div>
+								<div id="ra-ownerlabel" class="username username-3gJmXY size18-3EXdSj weightSemiBold-NJexzi" style="color: rgb(150, 150, 150); padding-top: 20px">Owner unknown</div>
 							</div>
 						</header>
 					</div>
@@ -222,21 +222,21 @@ class GuildAndFriendRemovalAlerts {
 			if(!friendIDs.includes(friend.id)){
 				if(!$("#ra-alertwindow").length){
 					app.append(`<div id="ra-alertwindow">
-					   <div class="backdrop-2ohBEd" style="opacity: 0.85; background-color: rgb(0, 0, 0); transform: translateZ(0px);" onclick="$(this).parent().remove();"></div>
-					   <div id="ra-modal" class="modal-2LIEKY" style="opacity: 1; overflow-y: auto; justify-content: flex-start;"></div>
+					   <div class="backdrop-1ocfXc" style="opacity: 0.85; background-color: rgb(0, 0, 0); transform: translateZ(0px);" onclick="$(this).parent().remove();"></div>
+					   <div id="ra-modal" class="modal-1UGdnR" style="opacity: 1; overflow-y: auto; justify-content: flex-start;"></div>
 					</div>`);
 				}
 				$("#ra-modal").append(`
-					<div class="inner-1_1f7b ra-frienditem" style="margin: 20px; min-height: 134px;">
-					<div class="topSectionNormal-2LlRG1">
-						<header class="header-2Lg0Oe flex-3B1Tl4 alignCenter-3VxkQP">
-							<div class="avatar-1BXaQj profile-3z9uol avatar-4g_GO6"><img id="ra-avatar" src="/assets/f046e2247d730629309457e902d5c5b3.svg" height="90" width="90"></div>
-							<div class="headerInfo-Gkqcz9">
+					<div class="inner-1JeGVc ra-frienditem" style="margin: 20px; min-height: 134px;">
+					<div class="topSectionNormal-2-vo2m">
+						<header class="header-QKLPzZ flex-1O1GKY alignCenter-1dQNNs">
+							<div class="avatar-16XVId profile-ZOdGIb avatar-3EQepX"><img id="ra-avatar" src="/assets/f046e2247d730629309457e902d5c5b3.svg" height="90" width="90"></div>
+							<div class="headerInfo-30uryT">
 								<div class="nameTag-2n-N0D userSelectText-wz4t4g nameTag-26T3kW">
-									<span id="ra-taglabel" class="username username-24t9uh size18-ZM4Qv- weightSemiBold-T8sxWH" style="padding-right: 7px;">Unknown</span>
-									<span class="username username-24t9uh size18-ZM4Qv- weightSemiBold-T8sxWH" style="float: right; cursor: pointer;" onclick="$(this.parentElement.parentElement.parentElement.parentElement.parentElement).remove();">X</span>
+									<span id="ra-taglabel" class="username username-3gJmXY size18-3EXdSj weightSemiBold-NJexzi" style="padding-right: 7px;">Unknown</span>
+									<span class="username username-3gJmXY size18-3EXdSj weightSemiBold-NJexzi" style="float: right; cursor: pointer;" onclick="$(this.parentElement.parentElement.parentElement.parentElement.parentElement).remove();">X</span>
 								</div>
-								<div class="username username-24t9uh size18-ZM4Qv- weightSemiBold-T8sxWH" style="color: rgb(150, 150, 150); padding-top: 20px">Friend was removed.</div>
+								<div class="username username-3gJmXY size18-3EXdSj weightSemiBold-NJexzi" style="color: rgb(150, 150, 150); padding-top: 20px">Friend was removed.</div>
 							</div>
 						</header>
 					</div>

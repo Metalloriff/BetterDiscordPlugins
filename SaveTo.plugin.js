9c0169f675d470108b08766686f6e8045ac76d89
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-05-04 19:13:39 -0400

    v0.1.2

--- SaveTo.plugin.js
+++ SaveTo.plugin.js
@@ -10,41 +10,176 @@ class SaveTo {
         this.data;
 
         this.defaultSettings = {
-            fileNameType : "original"
+            fileNameType : "original",
+            sortMode : "a-z"
         };
         this.settings;
 
         this.classes;
+        this.selectedFolder = -1;
 
 	}
 	
     getName() { return "Save To"; }
     getDescription() { return "Allows you to save images, videos, files, server icons and user avatars to your defined folders, or browse to a folder, via the context menu."; }
-    getVersion() { return "0.0.1"; }
+    getVersion() { return "0.1.2"; }
 	getAuthor() { return "Metalloriff"; }
 	getChanges() {
 		return {
-			
+            "0.1.2" : 
+            `
+                Added sort mode settings, including custom sorting.
+                You can now add, remove and modify folders via the settings menu.
+            `
 		};
     }
     
     getSettingsPanel() {
 
-        var date = new Date();
+        setTimeout(() => {
 
-        Metalloriff.Settings.addRadioGroup("st-file-name-type", "File name type:", [
-            { title : "Original", value : "original", description : `Example: unknown.png` },
-            { title : "Date", value : "date", description : `Example: ${date.toLocaleDateString().split("/").join("-")} ${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}.png` },
-            { title : "Random numbers", value : "random", description : `Example: ${date.getMonth()}${date.getDay()}${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}.png` },
-            { title : "Original + random numbers", value : "original+random", description : `Example: unknown ${date.getMonth()}${date.getDay()}${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}.png` }
-        ], this.settings.fileNameType, (e, choiceItem) => {
-            this.settings.fileNameType = choiceItem.value;
-            PluginUtilities.saveSettings("SaveTo", this.settings);
-        });
+            var date = new Date();
+
+            var refreshFolders = () => {
+
+                var fields = new Array();
+
+                for(let i = 0; i < this.data.folders.length; i++) {
+
+                    var textField = Metalloriff.Settings.Elements.createTextField("", "text", this.data.folders[i].path, e => {
+                        var name = e.target.value.substring(e.target.value.split("\\").join("/").lastIndexOf("/") + 1, e.target.value.length);
+                        if(e.target.value.trim().length == 0 || name.length == 0) {
+                            this.data.folders.splice(i, 1);
+                            e.target.parentElement.outerHTML = "";
+                        } else {
+                            this.data.folders[i].path = e.target.value;
+                            this.data.folders[i].name = name;
+                        }
+                        refreshFolders();
+                        PluginUtilities.saveData("SaveTo", "data", this.data);
+                    }, "focusout", "5px");
+
+                    textField.insertAdjacentElement("beforeend", Metalloriff.Settings.Elements.createButton("Browse", e => {
+                        $("#st-folder-upload").remove();
+                        $(".app").append(`<input id="st-folder-upload" style="display:none;" type="file" webkitdirectory directory></input>`);
+                        var fileBrowser = document.getElementById("st-folder-upload");
+                        fileBrowser.click();
+                        fileBrowser.addEventListener("change", () => {
+                            e.target.parentElement.querySelector("input").value = fileBrowser.files[0].path;
+                            this.data.folders[i].path = fileBrowser.files[0].path;
+                            this.data.folders[i].name = fileBrowser.files[0].path.substring(fileBrowser.files[0].path.split("\\").join("/").lastIndexOf("/") + 1, fileBrowser.files[0].path.length);
+                            $("#st-folder-upload").remove();
+                        });
+                        refreshFolders();
+                        PluginUtilities.saveData("SaveTo", "data", this.data);
+                    }, "float:right;"));
+
+                    var positionField = Metalloriff.Settings.Elements.createTextField("", "number", this.data.folders[i].position, e => {
+                        if(e.target.value.length == 0) e.target.value = this.data.folders[i].position;
+                        else this.data.folders[i].position = e.target.value;
+                        refreshFolders();
+                        PluginUtilities.saveData("SaveTo", "data", this.data);
+                    }, "focusout", "", { withParent : false }).querySelector("input");
+
+                    positionField.style.paddingRight = "10px";
+                    positionField.style.width = "100px";
+                    positionField.style.float = "left";
+
+                    textField.setAttribute("data-path", this.data.folders[i].path);
+                    textField.setAttribute("data-name", this.data.folders[i].name);
+                    textField.setAttribute("data-priority", this.data.folders[i].position);
+
+                    textField.querySelector("input").style.width = "430px";
+
+                    textField.addEventListener("click", () => this.selectedFolder = i);
+
+                    textField.insertAdjacentElement("afterbegin", positionField);
+
+                    fields.push(textField);
+
+                }
+
+                if(this.settings.sortMode == "a-z" || this.settings.sortMode == "z-a") fields = fields.sort((x, y) => {
+                    if(x.getAttribute("data-name").toLowerCase() < y.getAttribute("data-name").toLowerCase()) return -1;
+                    if(x.getAttribute("data-name").toLowerCase() > y.getAttribute("data-name").toLowerCase()) return 1;
+                    return 0;
+                });
+
+                if(this.settings.sortMode == "z-a" || this.settings.sortMode == "new-old") fields = fields.reverse();
+
+                if(this.settings.sortMode == "custom") fields = fields.sort((x, y) => {
+                    if(parseFloat(x.getAttribute("data-priority")) > parseFloat(y.getAttribute("data-priority"))) return -1;
+                    if(parseFloat(x.getAttribute("data-priority")) < parseFloat(y.getAttribute("data-priority"))) return 1;
+                    return 0;
+                });
+
+                document.getElementById("st-folders").innerHTML = "";
+
+                for(let i = 0; i < fields.length; i++) document.getElementById("st-folders").insertAdjacentElement("beforeend", fields[i]);
+
+            };
+
+            Metalloriff.Settings.pushElement(Metalloriff.Settings.Elements.createRadioGroup("st-file-name-type", "File name type:", [
+                { title : "Original", value : "original", description : `Example: unknown.png` },
+                { title : "Date", value : "date", description : `Example: ${date.toLocaleDateString().split("/").join("-")} ${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}.png` },
+                { title : "Random numbers", value : "random", description : `Example: ${date.getMonth()}${date.getDay()}${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}.png` },
+                { title : "Original + random numbers", value : "original+random", description : `Example: unknown ${date.getMonth()}${date.getDay()}${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}.png` }
+            ], this.settings.fileNameType, (e, choiceItem) => {
+                this.settings.fileNameType = choiceItem.value;
+                PluginUtilities.saveSettings("SaveTo", this.settings);
+            }), this.getName());
+
+            Metalloriff.Settings.pushElement(Metalloriff.Settings.Elements.createRadioGroup("st-sort-type", "Sort by:", [
+                { title : "A - Z", value : "a-z" },
+                { title : "Z - A", value : "z-a" },
+                { title : "Newest - Oldest", value : "new-old" },
+                { title : "Oldest - Newest", value : "old-new" },
+                { title : "By priority", value : "custom" }
+            ], this.settings.sortMode, (e, choiceItem) => {
+                this.settings.sortMode = choiceItem.value;
+                refreshFolders();
+                PluginUtilities.saveSettings("SaveTo", this.settings);
+            }), this.getName());
+
+            var folderLabels = document.createElement("div");
+
+            folderLabels.insertAdjacentHTML("beforeend", `<h5 style="color:white;padding-bottom:10px;padding-top:20px;">Folders:</h5>`);
+            folderLabels.insertAdjacentHTML("beforeend", `<h5 style="color:white;padding-bottom:10px;width:100px;display:inline-block;">Prioirty</h5>`);
+            folderLabels.insertAdjacentHTML("beforeend", `<h5 style="color:white;padding-bottom:10px;width:430px;display:inline-block;">Path</h5>`);
+
+            Metalloriff.Settings.pushElement(folderLabels, this.getName());
+
+            var foldersParentDiv = document.createElement("div");
+
+            foldersParentDiv.setAttribute("id", "st-folders");
+
+            Metalloriff.Settings.pushElement(foldersParentDiv, this.getName());
+
+            refreshFolders();
+
+            Metalloriff.Settings.pushHTML(`<div id="st-settings-buttons" style="text-align:center;padding-top:20px;"></div>`, this.getName());
+
+            var buttonParent = document.getElementById("st-settings-buttons");
+
+            buttonParent.insertAdjacentElement("beforeend", Metalloriff.Settings.Elements.createButton("Add Folder", () => {
+                this.browseForFolder(() => refreshFolders());
+            }));
+
+            buttonParent.insertAdjacentElement("beforeend", Metalloriff.Settings.Elements.createButton("Remove Selected Folder", () => {
+                this.data.folders.splice(this.selectedFolder, 1);
+                refreshFolders();
+                PluginUtilities.saveData("SaveTo", "data", this.data);
+            }, "margin-left:15px;", { id : "st-settings-remove-folder" }));
+
+            buttonParent.insertAdjacentElement("beforeend", Metalloriff.Settings.Elements.createButton("Open Selected Folder", () => {
+                window.open(`file:///${this.data.folders[this.selectedFolder].path}`);
+            }, "margin-left:15px;", { id : "st-settings-open-folder"}));
+
+        }, 0);
 
         return `
         
-            ${Metalloriff.Settings.pluginNameLabel(this.getName())}
+            ${Metalloriff.Settings.Elements.pluginNameLabel(this.getName())}
 
             <div id="metalloriff-plugin-settings"></div>
         
@@ -72,6 +207,8 @@ class SaveTo {
 	initialize() {
 
         PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/SaveTo.plugin.js");
+        
+        $("#NeatoBurritoLibrary").remove();
 
 		var lib = document.getElementById("NeatoBurritoLibrary");
 		if(lib == undefined) {
@@ -84,6 +221,26 @@ class SaveTo {
         } else { this.onLibLoaded(); }
         
         this.data = PluginUtilities.loadData("SaveTo", "data", this.defaultData);
+
+        var updated = new Array();
+
+        for(var i = 0; i < this.data.folders.length; i++) {
+
+            if(typeof this.data.folders[i] !== "object") {
+
+                var p = this.data.folders[i];
+
+                updated.push( {
+                    path : p,
+                    name : p.substring(p.split("\\").join("/").lastIndexOf("/") + 1, p.length),
+                    position : i
+                });
+
+            }
+
+        }
+
+        if(updated.length > 0) this.data.folders = updated;
 
         this.settings = PluginUtilities.loadSettings("SaveTo", this.defaultSettings);
 
@@ -98,6 +255,8 @@ class SaveTo {
                 this.onContextMenu(e);
             }, 0);
         });
+
+        Metalloriff.Changelog.compareVersions(this.getName(), this.getChanges());
 
     }
 
@@ -159,7 +318,7 @@ class SaveTo {
 
         url = url.replace(".webp", ".png");
 
-        var folderNames = Array.from(this.data.folders, x => x.substring(x.split("\\").join("/").lastIndexOf("/") + 1, x.length)).sort((x, y) => { if(x < y) return -1; if(x > y) return 1; return 0; }), fileName = url.substring(url.lastIndexOf("/") + 1, url.length), fileExtension = url.substring(url.lastIndexOf("."), url.length);
+        var fileName = url.substring(url.lastIndexOf("/") + 1, url.length), fileExtension = url.substring(url.lastIndexOf("."), url.length);
             
         var date = new Date();
 
@@ -171,33 +330,39 @@ class SaveTo {
         
         var options = new PluginContextMenu.Menu(false);
         options.addItems(new PluginContextMenu.TextItem("Remove Folder", { callback : e => {
-            this.data.folders.splice(this.data.folders.findIndex(x => x.endsWith(e.currentTarget.parentElement.parentElement.innerText.split("\n")[0])), 1);
+            this.data.folders.splice(this.data.folders.findIndex(x => x.name == e.currentTarget.parentElement.parentElement.innerText.split("\n")[0]), 1);
             PluginUtilities.saveData("SaveTo", "data", this.data);
             $(e.currentTarget.parentElement.parentElement).remove();
         }})).addItems(new PluginContextMenu.TextItem("Open Folder", { callback : e => {
-            window.open("file:///" + this.data.folders.find(x => x.endsWith(e.currentTarget.parentElement.parentElement.innerText.split("\n")[0])));
+            window.open("file:///" + this.data.folders.find(x => x.name == e.currentTarget.parentElement.parentElement.innerText.split("\n")[0]).path);
         }})).addItems(new PluginContextMenu.TextItem("Save To Folder", { callback : e => {
             e.target.parentElement.click();
         }}));
 
-        for(var i = 0; i < folderNames.length; i++) {;
-            menu.addItems(new PluginContextMenu.SubMenuItem(folderNames[i], options, { callback : e => {
-                Metalloriff.downloadFile(url, this.data.folders.find(x => x.endsWith(e.currentTarget.innerText.split("\n")[0])), fileName);
+        var sorted = this.data.folders;
+
+        if(this.settings.sortMode == "a-z" || this.settings.sortMode == "z-a") sorted = sorted.sort((x, y) => {
+            if(x.name.toLowerCase() < y.name.toLowerCase()) return -1;
+            if(x.name.toLowerCase() > y.name.toLowerCase()) return 1;
+            return 0;
+        });
+
+        if(this.settings.sortMode == "z-a" || this.settings.sortMode == "old-new") sorted = sorted.reverse();
+
+        if(this.settings.sortMode == "custom") sorted = sorted.sort((x, y) => {
+            if(x.position > y.position) return -1;
+            if(x.position < y.position) return 1;
+            return 0;
+        });
+
+        for(let i = 0; i < this.data.folders.length; i++) {;
+            menu.addItems(new PluginContextMenu.SubMenuItem(this.data.folders[i].name, options, { callback : e => {
+                Metalloriff.downloadFile(url, this.data.folders[i].path, fileName);
             }}));
         }
 
         menu.addItems(new PluginContextMenu.ItemGroup().addItems(new PluginContextMenu.TextItem("Add Folder", { callback : () => {
-            $("#st-folder-upload").remove();
-            $(".app").append(`<input id="st-folder-upload" style="display:none;" type="file" webkitdirectory directory></input>`);
-            var fileBrowser = document.getElementById("st-folder-upload");
-            fileBrowser.click();
-            fileBrowser.addEventListener("change", () => {
-                if(this.data.folders.indexOf(fileBrowser.files[0].path) == -1) {
-                    this.data.folders.push(fileBrowser.files[0].path);
-                    PluginUtilities.saveData("SaveTo", "data", this.data);
-                }
-                $("#st-folder-upload").remove();
-            });
+            this.browseForFolder();
         }})).addItems(new PluginContextMenu.TextItem("Browse", { callback : () => {
             $("#st-folder-upload").remove();
             $(".app").append(`<input id="st-folder-upload" style="display:none;" type="file" webkitdirectory directory></input>`);
@@ -215,6 +380,40 @@ class SaveTo {
         var subMenu = new PluginContextMenu.SubMenuItem(saveLabel, menu);
 
         $(`.${this.classes.itemGroup}`).last().append(subMenu.element);
+
+    }
+
+    browseForFolder(selected) {
+        
+        $("#st-folder-upload").remove();
+
+        $(".app").append(`<input id="st-folder-upload" style="display:none;" type="file" webkitdirectory directory></input>`);
+
+        var fileBrowser = document.getElementById("st-folder-upload");
+
+        fileBrowser.click();
+
+        fileBrowser.addEventListener("change", () => {
+
+            var p = fileBrowser.files[0].path;
+
+            if(this.data.folders.findIndex(x => x.path == p) == -1) {
+
+                this.data.folders.push({
+                    path : p,
+                    name : p.substring(p.split("\\").join("/").lastIndexOf("/") + 1, p.length),
+                    position : this.data.folders.length
+                });
+
+                PluginUtilities.saveData("SaveTo", "data", this.data);
+
+            }
+
+            $("#st-folder-upload").remove();
+
+            if(selected != undefined) selected();
+
+        });
 
     }
 	

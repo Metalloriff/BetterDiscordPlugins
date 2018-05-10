e59b160bbaaa9be686687adb459cb0a2f04f1e15
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-05-10 09:36:37 -0400

    v0.3.3

--- SendBDEmotes.plugin.js
+++ SendBDEmotes.plugin.js
@@ -4,7 +4,7 @@ class SendBDEmotes {
 	
     getName() { return "Send BD Emotes"; }
     getDescription() { return "Allows you to enclose Better Discord emotes in square brackets to send them as a higher resolution link that all users can see. Example: [forsenE]. You can also do [EmoteChannelName.EmoteName]. Example: [FrankerFaceZ.SeemsGood]."; }
-    getVersion() { return "0.2.3"; }
+    getVersion() { return "0.3.3"; }
     getAuthor() { return "Metalloriff"; }
 
     load() {}
@@ -24,32 +24,35 @@ class SendBDEmotes {
 	
 	initialize(){
 		PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/SendBDEmotes.plugin.js");
-        this.onSwitch();
+		this.hasPermission = InternalUtilities.WebpackModules.findByUniqueProperties(["can"]).can;
+		this.uploadFile = InternalUtilities.WebpackModules.findByUniqueProperties(["upload"]).upload;
+		this.onSwitch();
 	}
 
     onSwitch(){
-		var chatboxJQ = $("textarea");
+		var chatboxJQ = $(".chat textarea");
 		if(chatboxJQ.length){
 			var chatbox = chatboxJQ[0];
 			chatboxJQ.on("keydown.SendBDEmotes", e => {
 				if(e.which == 13 && !e.shiftKey && chatbox.value){
-                    var chatboxValue = chatbox.value, words = chatboxValue.split(" "), lastWord = words[words.length - 1];
+                    var words = chatbox.value.split(" "), lastWord = words[words.length - 1];
                     if(lastWord.startsWith("[") && lastWord.endsWith("]")){
                         var emoteName = lastWord.substring(1, lastWord.length - 1), emote = window.bdEmotes.TwitchGlobal[emoteName] || window.bdEmotes.TwitchSubscriber[emoteName] || window.bdEmotes.BTTV[emoteName] || window.bdEmotes.FrankerFaceZ[emoteName] || window.bdEmotes.BTTV2[emoteName];
 						if(emoteName.includes(".")){
 							var sourceAndName = emoteName.split(".");
 							emote = window.bdEmotes[sourceAndName[0]][sourceAndName[1]];
 						}
-                        if(emote != undefined){
-                            var i = emote.lastIndexOf("1");
 
-							words[words.length - 1] = emote.substring(0, i) + (emote.includes("cdn.frankerfacez.com") ? "4" : "3") + emote.substring(i + 1);
-							
-							chatboxValue = words.join(" ");
+                        if(emote != undefined){
+							var i = emote.lastIndexOf("1"), url = emote.substring(0, i) + "4" + emote.substring(i + 1), selectedChannelId = Metalloriff.getSelectedChannel().id;
 
                             chatbox.focus();
                             chatbox.select();
-                            document.execCommand("insertText", false, chatboxValue);
+							document.execCommand("insertText", false, chatbox.value.split(lastWord).join(""));
+							
+							Metalloriff.requestFile(url, emoteName + ".png", file => {
+								this.uploadFile(selectedChannelId, file);
+							});
                         }
                     }
 				}
@@ -58,7 +61,7 @@ class SendBDEmotes {
     }
 	
     stop() {
-		var chatbox = $("textarea");
+		var chatbox = $(".chat textarea");
 		if(chatbox)
 			chatbox.off("keydown.SendBDEmotes");
     }

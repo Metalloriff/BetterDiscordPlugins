bb47f5d8ff9387cc8aabc57b823bc22c5aab2c01
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-05-11 16:07:55 -0400

    v0.3.4

--- SendBDEmotes.plugin.js
+++ SendBDEmotes.plugin.js
@@ -4,9 +4,9 @@ class SendBDEmotes {
 	
     getName() { return "Send BD Emotes"; }
     getDescription() { return "Allows you to enclose Better Discord emotes in square brackets to send them as a higher resolution link that all users can see. Example: [forsenE]. You can also do [EmoteChannelName.EmoteName]. Example: [FrankerFaceZ.SeemsGood]."; }
-    getVersion() { return "0.3.3"; }
+    getVersion() { return "0.3.4"; }
     getAuthor() { return "Metalloriff"; }
-
+	
     load() {}
 
     start() {
@@ -21,11 +21,47 @@ class SendBDEmotes {
 		if (typeof window.ZeresLibrary !== "undefined") this.initialize();
 		else libraryScript.addEventListener("load", () => { this.initialize(); });
 	}
+
+	getSettingsPanel() {
+
+		setTimeout(() => {
+			
+			Metalloriff.Settings.pushElement(Metalloriff.Settings.Elements.createRadioGroup("sbde-size", "Emote size:", [
+				{ title : "Small", description : "BD default", value : 1 },
+				{ title : "Medium", value : 2 },
+				{ title : "Large", value : 4 }
+			], this.settings.emoteSize, (e, choice) => {
+				this.settings.emoteSize = choice.value;
+				this.saveSettings();
+			}), this.getName());
+
+		}, 0);
+
+		return Metalloriff.Settings.Elements.pluginNameLabel(this.getName());
+
+	}
+
+	saveSettings() { PluginUtilities.saveSettings("SendBDEmotes", this.settings); }
 	
 	initialize(){
 		PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/SendBDEmotes.plugin.js");
+
+		var lib = document.getElementById("NeatoBurritoLibrary");
+		if(lib == undefined) {
+			lib = document.createElement("script");
+			lib.setAttribute("type", "text/javascript");
+			lib.setAttribute("src", "https://www.dropbox.com/s/cxhekh6y9y3wqvo/NeatoBurritoLibrary.js?raw=1");
+			lib.setAttribute("id", "NeatoBurritoLibrary");
+			document.head.appendChild(lib);
+		}
+		
 		this.hasPermission = InternalUtilities.WebpackModules.findByUniqueProperties(["can"]).can;
 		this.uploadFile = InternalUtilities.WebpackModules.findByUniqueProperties(["upload"]).upload;
+
+		this.settings = PluginUtilities.loadSettings("SendBDEmotes", {
+			emoteSize : 4
+		});
+
 		this.onSwitch();
 	}
 
@@ -44,15 +80,24 @@ class SendBDEmotes {
 						}
 
                         if(emote != undefined){
-							var i = emote.lastIndexOf("1"), url = emote.substring(0, i) + "4" + emote.substring(i + 1), selectedChannelId = Metalloriff.getSelectedChannel().id;
+							var i = emote.lastIndexOf("1"), size = this.settings.emoteSize, selectedChannelId = Metalloriff.getSelectedChannel().id;
 
                             chatbox.focus();
                             chatbox.select();
 							document.execCommand("insertText", false, chatbox.value.split(lastWord).join(""));
 							
-							Metalloriff.requestFile(url, emoteName + ".png", file => {
+							var request = () => Metalloriff.requestFile(emote.substring(0, i) + size + emote.substring(i + 1), emoteName + ".png", file => {
+								if(file.size < 100) {
+									if(size > 1) {
+										size--;
+										request();
+									}
+									return;
+								}
 								this.uploadFile(selectedChannelId, file);
 							});
+
+							request();
                         }
                     }
 				}

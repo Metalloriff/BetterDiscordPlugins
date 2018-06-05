f8d0c100723793d1c56582a768fcb667316d8d73
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-06-05 13:10:22 -0400

    v0.4.2

--- TheClapBestClapPluginClapEver.plugin.js
+++ TheClapBestClapPluginClapEver.plugin.js
@@ -3,8 +3,8 @@
 class TheClapBestClapPluginClapEver {
 	
     getName() { return "The Clap Best Clap Plugin Clap Ever"; }
-	getDescription() { return "Literally the most useless plugin ever. Put \"clapclap:\" at the first of your message to replace spaces with clap emojis. You can also do \"clapclap(some_emote_name):\" to use custom emotes, \"ra:\" to replace all characters with regional indicators, and \"reverse:\" to reverse the message."; }
-    getVersion() { return "0.3.2"; }
+	getDescription() { return `Literally the most useless and cancerous plugin ever. Put "clapclap:" at the first of your message to replace spaces with clap emojis. You can also do "clapclap(some_emote_name):" to use custom emotes, "superclapclap" for every character instead of every space, "ra:" to replace all characters with regional indicators, "reverse:" to reverse the message, and "owo:" for complete fucking cancer.`; }
+    getVersion() { return "0.4.2"; }
     getAuthor() { return "Metalloriff"; }
 
     load() {}
@@ -23,54 +23,85 @@ class TheClapBestClapPluginClapEver {
 	}
 	
 	initialize(){
+
 		PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/TheClapBestClapPluginClapEver.plugin.js");
-		this.attach();
+
+		this.onChatInput = e => {
+
+			let chatbox = e.target;
+
+			if(e.which == 13 && !e.shiftKey && chatbox.value){
+
+				let chatboxValue = chatbox.value.trim();
+
+				if(chatboxValue.startsWith("clapclap") || chatboxValue.startsWith("superclapclap")){
+
+					let getClapClap = chatboxValue.substring(0, chatboxValue.indexOf(":") + 1), emote = "clap", definedEmote = getClapClap.substring(getClapClap.indexOf("(") + 1, getClapClap.indexOf(")"));
+
+					if(definedEmote) emote = definedEmote;
+
+					emote = " :" + emote  + ": ";
+
+					chatboxValue = chatboxValue.replace(getClapClap, "").split(chatboxValue.startsWith("super") ? "" : " ").join(emote);
+
+					if(chatboxValue.startsWith(emote)) chatboxValue += emote;
+					
+				}
+
+				if(chatboxValue.startsWith("ra:")){
+
+					let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", temp = chatboxValue.split(" ").join("\t");
+
+					for(let i = 0; i < alphabet.length; i++) temp = temp.replace("ra:", "").split(alphabet[i]).join("[a" + i + "]");
+					for(let i = 0; i < alphabet.length; i++) temp = temp.split("[a" + i + "]").join(":regional_indicator_" + alphabet[i].toLowerCase() + ": ");
+
+					temp = temp.split("?").join(":question:");
+
+					chatboxValue = temp;
+
+				}
+
+				if(chatboxValue.startsWith("reverse:")) chatboxValue = chatboxValue.replace("reverse:", "").split("").reverse().join("");
+
+				if(chatboxValue.startsWith("owo:")) {
+
+					let cancer = ["owo", "OwO", "uwu", "UwU", ">w<", "^w^"];
+
+					chatboxValue = chatboxValue.replace("owo:", "").trim();
+
+					chatboxValue = chatboxValue.split("r").join("w").split("R").join("W").split("l").join("w").split("L").join("W").split(" n").join(" ny").split(" N").join(" Ny").split("ove").join("uv").split("OVE").join("UV");
+
+					chatboxValue += " " + cancer[cancer.length * Math.random() << 0];
+
+					if(chatbox.value == chatbox.value.toUpperCase()) chatboxValue = chatboxValue.toUpperCase();
+
+				}
+				
+				chatbox.select();
+				document.execCommand("insertText", false, chatboxValue);
+
+			}
+
+		};
+
+		this.onSwitch();
+
 	}
 	
 	onSwitch(){
-		this.attach();
-	}
-	
-	attach(){
-		var chatboxJQ = $("textarea");
-		if(chatboxJQ.length){
-			var chatbox = chatboxJQ[0];
-			chatboxJQ.on("keydown.ClapClap", e => {
-				if(e.which == 13 && !e.shiftKey && chatbox.value){
-					var chatboxValue = chatbox.value;
-					if(chatboxValue.startsWith("clapclap")){
-						var getClapClap = chatboxValue.substring(0, chatboxValue.indexOf(":") + 1), emote = "clap", definedEmote = getClapClap.substring(getClapClap.indexOf("(") + 1, getClapClap.indexOf(")"));
-						if(definedEmote)
-							emote = definedEmote;
-						emote = " :" + emote  + ": ";
-						chatboxValue = chatboxValue.replace(getClapClap, "").split(" ").join(emote);
-						if(chatboxValue.startsWith(emote))
-							chatboxValue += emote;
-					}
-					if(chatboxValue.startsWith("ra:")){
-						var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", temp = chatboxValue;
-						temp = temp.split(" ").join("\t");
-						for(var i = 0; i < alphabet.length; i++)
-							temp = temp.replace("ra:", "").split(alphabet[i]).join("[a" + i + "]");
-						for(var i = 0; i < alphabet.length; i++)
-							temp = temp.split("[a" + i + "]").join(":regional_indicator_" + alphabet[i].toLowerCase() + ": ");
-						temp = temp.split("?").join(":question:");
-						chatboxValue = temp;
-					}
-					if(chatboxValue.startsWith("reverse:"))
-						chatboxValue = chatboxValue.replace("reverse:", "").split("").reverse().join("");
-					chatbox.focus();
-					chatbox.select();
-					document.execCommand("insertText", false, chatboxValue);
-				}
-			});
-		}
+		
+		let chat = document.getElementsByClassName("chat")[0], chatbox = chat ? chat.getElementsByTagName("textarea")[0] : undefined;
+
+		if(chatbox) chatbox.addEventListener("keydown", this.onChatInput);
+
 	}
 	
     stop() {
-		var chatbox = $("textarea");
-		if(chatbox)
-			chatbox.off("keydown.ClapClap");
+		
+		let chat = document.getElementsByClassName("chat")[0], chatbox = chat ? chat.getElementsByTagName("textarea")[0] : undefined;
+
+		if(chatbox) chatbox.removeEventListener("keydown", this.onChatInput);
+
 	}
 	
 }

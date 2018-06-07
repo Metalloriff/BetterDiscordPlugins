4bb318aa92fe1d875ec3f224a82964fa1873505f
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-06-07 11:34:09 -0400

    v0.1.3

--- SelectedChannelNotifications.plugin.js
+++ SelectedChannelNotifications.plugin.js
@@ -2,133 +2,15 @@
 
 class SelectedChannelNotifications {
 	
-	getSettingsPanel(){
-		if(!$(".plugin-settings").length)
-			setTimeout(e => { this.getSettingsPanel(e); }, 100);
-		else
-			this.createSettingsPanel();
-	}
-
-	createSettingsPanel(){
-		var panel = $(".plugin-settings");
-		if(panel.length){
-			panel.append(`
-
-                <style>
-                .scn-ticked {
-                    background-color: white;
-                    height: 60%;
-                    width: 60%;
-                    border-radius: 3px;
-                }
-                </style>
-
-				<h style="color: rgb(255, 255, 255); font-size: 30px; font-weight: bold;">Selected Channel Notifications by Metalloriff</h>
-				<br><br>
-
-				<p style="color: rgb(255, 255, 255); font-size: 20px;">Message notification sound:</p>
-                <input id="scn-messageSound" value="` + this.settings.messageSound + `" class="inputDefault-_djjkz input-cIJ7To size16-14cGz5">
-                
-                
-                <div style="padding-top: 20px;">
-                <div id="scn-checkbox-list" class="radioGroup-1GBvlr">
-
-                <div data-key="playSound" class="item-26Dhrx marginBottom8-AtZOdT horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG cardPrimaryEditable-3KtE4g card-3Qj_Yx" style="padding: 10px;border-radius: 0px !important;">
-                    <label class="checkboxWrapper-SkhIWG">
-                        <input type="checkbox" class="inputDefault-3JxKJ2 input-3ITkQf" value="on">
-                        <div class="checkbox-1ix_J3 flexCenter-3_1bcw flex-1O1GKY justifyCenter-3D2jYp alignCenter-1dQNNs box-mmYMsp">
-                        </div>
-                    </label>
-                    <div class="info-3LOr12">
-                        <div class="title-3BE6m5">Play sound</div>
-                    </div>
-                </div>
-
-                <div data-key="displayNotification" class="item-26Dhrx marginBottom8-AtZOdT horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG cardPrimaryEditable-3KtE4g card-3Qj_Yx" style="padding: 10px;border-radius: 0px !important;">
-                    <label class="checkboxWrapper-SkhIWG">
-                        <input type="checkbox" class="inputDefault-3JxKJ2 input-3ITkQf" value="on">
-                        <div class="checkbox-1ix_J3 flexCenter-3_1bcw flex-1O1GKY justifyCenter-3D2jYp alignCenter-1dQNNs box-mmYMsp">
-                        </div>
-                    </label>
-                    <div class="info-3LOr12">
-                        <div class="title-3BE6m5">Display notification</div>
-                    </div>
-                </div>
-
-                </div>
-                </div>
-
-				<div style="text-align: center;">
-				<br>
-				<button id="scn-reset-button" style="display: inline-block; margin-right: 25px;" type="button" class="button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeMedium-1AC_Sl grow-q77ONN">
-					<div class="contents-4L4hQM">Reset Settings</div>
-				</button>
-				<button id="scn-save-button" style="display: inline-block; margin-left: 25px;" type="button" class="button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeMedium-1AC_Sl grow-q77ONN">
-					<div class="contents-4L4hQM">Save Settings</div>
-				</button>
-                </div>
-                
-			`);
-            var messageSound = document.getElementById("scn-messageSound"),
-            playSound = document.getElementById("scn-playSound"),
-            displayNotification = document.getElementById("scn-displayNotification");
-            if(this.settings.playSound == true){ $(`[data-key="playSound"] > label > div`).append(`<div class="scn-ticked"></div>`); }
-            if(this.settings.displayNotification == true){ $(`[data-key="displayNotification"] > label > div`).append(`<div class="scn-ticked"></div>`); }
-            $("#scn-checkbox-list > div").on("click", e => {
-                var key = $(e.currentTarget).data("key");
-                this.settings[key] = !this.settings[key];
-                $(e.currentTarget).find(".scn-ticked").remove();
-                if(this.settings[key] == true){ $(`[data-key="` + key + `"] > label > div`).append(`<div class="scn-ticked"></div>`); }
-            });
-			$("#scn-reset-button").on("click", () => {
-                this.settings = this.defaultSettings;
-                $(".scn-ticked").remove();
-                $("#scn-checkbox-list > div > label > div").append(`<div class="scn-ticked"></div>`);
-                messageSound.value = "https://discordapp.com/assets/dd920c06a01e5bb8b09678581e29d56f.mp3";
-			});
-			$("#scn-save-button").on("click", () => {
-                this.settings = {
-                    messageSound : messageSound.value,
-                    playSound : $(`[data-key="playSound"]`).find(".scn-ticked").length > 0,
-                    displayNotification : $(`[data-key="displayNotification"]`).find(".scn-ticked").length > 0
-                };
-                this.saveSettings();
-                PluginUtilities.showToast("Settings saved!", { type : "success" });
-			});
-		}else
-			this.getSettingsPanel();
-	}
-	
-	saveSettings(){
-        PluginUtilities.saveSettings("SelectedChannelNotifications", this.settings);
-        this.onSwitch();
-	}
-	
-	constructor() {
-        this.channelModule;
-        this.notificationSettingsModule;
-        this.volumeModule;
-        this.currentGuild;
-        this.currentChannel;
-        this.messageObserver;
-        this.defaultSettings = {
-            messageSound : "https://discordapp.com/assets/dd920c06a01e5bb8b09678581e29d56f.mp3",
-            playSound : true,
-            displayNotification : true
-        };
-        this.settings;
-        this.audio = new Audio();
-        this.focused = true;
-	}
-	
     getName() { return "Selected Channel Notifications"; }
     getDescription() { return "Plays a sound and displays a notification (both optional) when Discord is minimized and a message is received in the selected channel."; }
-    getVersion() { return "0.0.3"; }
+    getVersion() { return "0.1.3"; }
     getAuthor() { return "Metalloriff"; }
 
     load() {}
 
     start() {
+
 		var libraryScript = document.getElementById('zeresLibraryScript');
 		if (!libraryScript) {
 			libraryScript = document.createElement("script");
@@ -138,54 +20,156 @@ class SelectedChannelNotifications {
 			document.head.appendChild(libraryScript);
 		}
 		if (typeof window.ZeresLibrary !== "undefined") this.initialize();
-		else libraryScript.addEventListener("load", () => { this.initialize(); });
+        else libraryScript.addEventListener("load", () => { this.initialize(); });
+        
 	}
 	
-	initialize(){
+	getSettingsPanel() {
+
+		setTimeout(() => {
+
+            Metalloriff.Settings.pushElement(Metalloriff.Settings.Elements.createTextField("Message notification sound", "text", this.settings.messageSound, e => {
+                this.settings.messageSound = e.target.value;
+                this.saveSettings();
+            }), this.getName());
+
+            Metalloriff.Settings.pushElement(Metalloriff.Settings.Elements.createToggleGroup("scn-toggles", "", [
+                { title : "Play sound on message", value : "playSound", setValue : this.settings.playSound },
+                { title : "Display notification on message", value : "displayNotification", setValue : this.settings.displayNotification }
+            ], choice => {
+                this.settings[choice.value] = !this.settings[choice.value];
+                this.saveSettings();
+            }), this.getName());
+
+		}, 0);
+
+        return Metalloriff.Settings.Elements.pluginNameLabel(this.getName());
+        
+	}
+	
+	saveSettings() {
+
+        PluginUtilities.saveSettings("SelectedChannelNotifications", this.settings);
+        
+        this.onSwitch();
+
+	}
+	
+	initialize() {
+
         PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/SelectedChannelNotifications.plugin.js");
-        this.settings = PluginUtilities.loadSettings("SelectedChannelNotifications", this.defaultSettings);
+
+        this.audio = new Audio();
+
+        this.notifications = [];
+
+        this.settings = PluginUtilities.loadSettings("SelectedChannelNotifications", {
+            messageSound : "https://discordapp.com/assets/dd920c06a01e5bb8b09678581e29d56f.mp3",
+            playSound : true,
+            displayNotification : true
+        });
+
         this.channelModule = DiscordModules.ChannelStore;
         this.notificationSettingsModule = InternalUtilities.WebpackModules.findByUniqueProperties(["getChannelMessageNotifications"]);
         this.volumeModule = InternalUtilities.WebpackModules.findByUniqueProperties(["getOutputVolume"]);
-        $(window).on("focus.SelectedChannelNotifications", () => { this.focused = true; });
-        $(window).on("blur.SelectedChannelNotifications", () => { this.focused = false; });
-        this.onSwitch();
+        
+        this.focused = true;
+
+        this.focus = () => {
+            this.focused = true;
+            for(let i = 0; i < this.notifications.length; i++) this.notifications[i].close();
+        };
+        this.unfocus = () => this.focused = false;
+
+        this.messageObserver = new MutationObserver(mutations => {
+            for(let i = 0; i < mutations.length; i++) {
+                if(mutations[i].addedNodes && mutations[i].addedNodes[0] && mutations[i].addedNodes[0].classList.contains("message-group")) {
+                    this.onMessageReceived();
+                }
+            }
+        });
+
+        window.addEventListener("focus", this.focus);
+        window.addEventListener("blur", this.unfocus);
+
+        var lib = document.getElementById("NeatoBurritoLibrary");
+		if(lib == undefined) {
+			lib = document.createElement("script");
+			lib.setAttribute("type", "text/javascript");
+			lib.setAttribute("src", "https://www.dropbox.com/s/cxhekh6y9y3wqvo/NeatoBurritoLibrary.js?raw=1");
+			lib.setAttribute("id", "NeatoBurritoLibrary");
+			document.head.appendChild(lib);
+		}
+        if(typeof window.Metalloriff !== "undefined") this.onLibLoaded();
+        else lib.addEventListener("load", () => { this.onLibLoaded(); });
+
     }
 
-    onSwitch(){
+    onLibLoaded() {
+
+        this.initialized = true;
+
+        this.onSwitch();
+
+    }
+
+    onSwitch() {
+
+        if(!this.initialized) return;
+
         this.currentGuild = PluginUtilities.getCurrentServer();
-        var messageScroller = $(".messages.scroller"), selectedChannel = $(".wrapperSelectedText-3dSUjC");
+        let messageScroller = document.getElementsByClassName("messages scroller")[0], selectedChannel = document.getElementsByClassName("wrapperSelectedText-3dSUjC")[0];
 
-        if(this.messageObserver != undefined){ this.messageObserver.disconnect(); }
+        this.messageObserver.disconnect();
 
-        if(this.currentGuild == undefined || !messageScroller.length || !selectedChannel.length){ return; }
+        if(this.currentGuild == undefined || messageScroller == undefined || selectedChannel == undefined) return;
 
-        this.currentChannel = ReactUtilities.getOwnerInstance(selectedChannel[0]).props.channel;
+        this.currentChannel = ReactUtilities.getOwnerInstance(selectedChannel).props.channel;
 
-        if(this.currentChannel == undefined || this.notificationSettingsModule.getChannelMessageNotifications(this.currentGuild.id, this.currentChannel.id) == 0){ return; }
+        if(this.currentChannel == undefined || this.notificationSettingsModule.getChannelMessageNotifications(this.currentGuild.id, this.currentChannel.id) == 0) return;
 
         this.audio.src = this.settings.messageSound;
         this.audio.volume = this.volumeModule.getOutputVolume() / 100;
 
-        this.messageObserver = new MutationObserver(() => {
-            if(!this.focused){
-                var latestMessageGroupMessages = ReactUtilities.getOwnerInstance($(".message-group").last()[0]).props.messages,
-                latestMessage = latestMessageGroupMessages[latestMessageGroupMessages.length - 1];
-                if(latestMessage.author.id != PluginUtilities.getCurrentUser().id && latestMessage.mentioned == false){
-                    if(this.settings.displayNotification == true){
-                        var notif = new Notification((latestMessage.nick == undefined ? latestMessage.author.username : latestMessage.nick) + " - #" + this.currentChannel.name, { silent : true, body : latestMessage.content, icon : latestMessage.author.getAvatarURL() });
-                    }
-                    if(this.settings.playSound == true){ this.audio.play(); }
+        this.messageObserver.observe(messageScroller, { childList : true });
+
+    }
+
+    onMessageReceived() {
+
+        if(!this.focused && Metalloriff.getLocalStatus() != "dnd") {
+
+            let messages = document.getElementsByClassName("message-group"),
+            latestMessageGroupMessages = ReactUtilities.getOwnerInstance(messages[messages.length - 1]).props.messages,
+            latestMessage = latestMessageGroupMessages[latestMessageGroupMessages.length - 1];
+
+            if(latestMessage.author.id != PluginUtilities.getCurrentUser().id && latestMessage.mentioned == false) {
+
+                if(this.settings.displayNotification == true) {
+
+                    let notif = new Notification((latestMessage.nick == undefined ? latestMessage.author.username : latestMessage.nick) + " - #" + this.currentChannel.name, { silent : true, body : latestMessage.content, icon : latestMessage.author.getAvatarURL() });
+
+                    let idx = this.notifications.push(notif) - 1;
+
+                    notif.onclose = () => this.notifications.splice(idx, 1);
+
                 }
+
+                if(this.settings.playSound == true) this.audio.play();
+
             }
-        });
-        this.messageObserver.observe(messageScroller[0], { childList : true });
+
+        }
+
     }
 	
     stop() {
-        if(this.messageObserver != undefined){ this.messageObserver.disconnect(); }
-        $(window).off("focus.SelectedChannelNotifications");
-        $(window).off("blur.SelectedChannelNotifications");
+
+        if(this.messageObserver != undefined) this.messageObserver.disconnect();
+
+        window.removeEventListener("focus", this.focus);
+        window.removeEventListener("blur", this.unfocus);
+
     }
 	
 }

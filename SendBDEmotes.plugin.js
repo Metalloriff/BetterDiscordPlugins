ba7306d911d37725c00f0d41f0e32405e1be4645
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-06-08 10:48:25 -0400

    v0.5.9

--- SendBDEmotes.plugin.js
+++ SendBDEmotes.plugin.js
@@ -4,7 +4,7 @@ class SendBDEmotes {
 	
     getName() { return "Send BD Emotes"; }
     getDescription() { return "Allows you to enclose Better Discord emotes in square brackets to send them as a higher resolution link that all users can see. Example: [forsenE]. You can also do [EmoteChannelName.EmoteName]. Example: [FrankerFaceZ.SeemsGood]. [EmoteName:size]. Example: [forsenE:1]. And [EmoteName_a] for animated emotes."; }
-    getVersion() { return "0.5.8"; }
+    getVersion() { return "0.5.9"; }
     getAuthor() { return "Metalloriff"; }
 	
     load() {}
@@ -60,14 +60,16 @@ class SendBDEmotes {
 
 		PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/SendBDEmotes.plugin.js");
 
-		var lib = document.getElementById("NeatoBurritoLibrary");
+		let lib = document.getElementById("NeatoBurritoLibrary");
 		if(lib == undefined) {
 			lib = document.createElement("script");
-			lib.setAttribute("type", "text/javascript");
-			lib.setAttribute("src", "https://www.dropbox.com/s/cxhekh6y9y3wqvo/NeatoBurritoLibrary.js?raw=1");
 			lib.setAttribute("id", "NeatoBurritoLibrary");
+			lib.setAttribute("type", "text/javascript");
+			lib.setAttribute("src", "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js");
 			document.head.appendChild(lib);
 		}
+        if(typeof window.Metalloriff !== "undefined") this.onLibLoaded();
+        else lib.addEventListener("load", () => { this.onLibLoaded(); });
 		
 		this.hasPermission = InternalUtilities.WebpackModules.findByUniqueProperties(["can"]).can;
 		this.uploadFile = InternalUtilities.WebpackModules.findByUniqueProperties(["upload"]).upload;

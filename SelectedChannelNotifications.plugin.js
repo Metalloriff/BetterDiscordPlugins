8c0f1d6358b000f671198101fef4f05c93d71cea
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-06-08 10:55:22 -0400

    v0.1.5

--- SelectedChannelNotifications.plugin.js
+++ SelectedChannelNotifications.plugin.js
@@ -4,7 +4,7 @@ class SelectedChannelNotifications {
 	
     getName() { return "Selected Channel Notifications"; }
     getDescription() { return "Plays a sound and displays a notification (both optional) when Discord is minimized and a message is received in the selected channel."; }
-    getVersion() { return "0.1.4"; }
+    getVersion() { return "0.1.5"; }
     getAuthor() { return "Metalloriff"; }
 
     load() {}
@@ -88,12 +88,12 @@ class SelectedChannelNotifications {
         window.addEventListener("focus", this.focus);
         window.addEventListener("blur", this.unfocus);
 
-        var lib = document.getElementById("NeatoBurritoLibrary");
+		let lib = document.getElementById("NeatoBurritoLibrary");
 		if(lib == undefined) {
 			lib = document.createElement("script");
-			lib.setAttribute("type", "text/javascript");
-			lib.setAttribute("src", "https://www.dropbox.com/s/cxhekh6y9y3wqvo/NeatoBurritoLibrary.js?raw=1");
 			lib.setAttribute("id", "NeatoBurritoLibrary");
+			lib.setAttribute("type", "text/javascript");
+			lib.setAttribute("src", "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js");
 			document.head.appendChild(lib);
 		}
         if(typeof window.Metalloriff !== "undefined") this.onLibLoaded();

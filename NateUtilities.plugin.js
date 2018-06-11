17d8ec9cea3045f3245b08f6af4836906442fc13
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-06-10 20:44:45 -0400

    v0.0.2

--- NateUtilities.plugin.js
+++ NateUtilities.plugin.js
@@ -4,7 +4,7 @@ class NateUtilities {
 	
     getName() { return "NateUtilities"; }
     getDescription() { return "For all of your ear and brain saving needs! If you don't know what this plugin is, it's not for you, just ignore it."; }
-    getVersion() { return "0.0.1"; }
+    getVersion() { return "0.0.2"; }
 	getAuthor() { return "Metalloriff"; }
 	getChanges() {
 		return {
@@ -16,16 +16,21 @@ class NateUtilities {
 
     start() {
 
-		var libraryScript = document.getElementById('zeresLibraryScript');
-		if (!libraryScript) {
-			libraryScript = document.createElement("script");
-			libraryScript.setAttribute("type", "text/javascript");
-			libraryScript.setAttribute("src", "https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js");
-			libraryScript.setAttribute("id", "zeresLibraryScript");
-			document.head.appendChild(libraryScript);
+        let libLoadedEvent = () => {
+            try{ this.onLibLoaded(); }
+            catch(err) { console.error(this.getName(), "fatal error, plugin could not be started!", err); }
+        };
+
+		let lib = document.getElementById("NeatoBurritoLibrary");
+		if(lib == undefined) {
+			lib = document.createElement("script");
+			lib.setAttribute("id", "NeatoBurritoLibrary");
+			lib.setAttribute("type", "text/javascript");
+			lib.setAttribute("src", "https://rawgit.com/NeatoLib/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js");
+			document.head.appendChild(lib);
 		}
-		if (typeof window.ZeresLibrary !== "undefined") this.initialize();
-		else libraryScript.addEventListener("load", () => { this.initialize(); });
+        if(typeof window.NeatoLib !== "undefined") libLoadedEvent();
+        else lib.addEventListener("load", libLoadedEvent);
 
 	}
 
@@ -33,7 +38,7 @@ class NateUtilities {
 
 		setTimeout(() => {
 
-            Metalloriff.Settings.pushElement(Metalloriff.Settings.Elements.createKeybindInput("local mute nate", this.settings.muteHotkey, newKey => {
+            NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createKeybindInput("local mute nate", this.settings.muteHotkey, newKey => {
                 this.unregisterKeybinds();
                 if(newKey) {
                     this.settings.muteHotkey = newKey;
@@ -42,7 +47,7 @@ class NateUtilities {
                 } else PluginUtilities.showToast("You did not input anything!", { type : "error" });
             }, { description : "For when your ears and/or brain need a break.", global : true }), this.getName());
 
-            Metalloriff.Settings.pushElement(Metalloriff.Settings.Elements.createKeybindInput("server mute nate", this.settings.serverMuteHotkey, newKey => {
+            NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createKeybindInput("server mute nate", this.settings.serverMuteHotkey, newKey => {
                 this.unregisterKeybinds();
                 if(newKey) {
                     this.settings.serverMuteHotkey = newKey;
@@ -51,7 +56,7 @@ class NateUtilities {
                 } else PluginUtilities.showToast("You did not input anything!", { type : "error" });
             }, { description : "For when you feel like being a hero.", global : true }), this.getName());
 
-            Metalloriff.Settings.pushElement(Metalloriff.Settings.Elements.createKeybindInput("server deafen nate", this.settings.serverDeafenHotkey, newKey => {
+            NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createKeybindInput("server deafen nate", this.settings.serverDeafenHotkey, newKey => {
                 this.unregisterKeybinds();
                 if(newKey) {
                     this.settings.serverDeafenHotkey = newKey;
@@ -60,47 +65,32 @@ class NateUtilities {
                 } else PluginUtilities.showToast("You did not input anything!", { type : "error" });
             }, { description : "For when you need to talk behind Nate's back.", global : true }), this.getName());
 
-            Metalloriff.Settings.pushChangelogElements(this);
+            NeatoLib.Settings.pushChangelogElements(this);
 
 		}, 0);
 
-		return Metalloriff.Settings.Elements.pluginNameLabel(this.getName());
+		return NeatoLib.Settings.Elements.pluginNameLabel(this.getName());
 		
 	}
 
 	saveSettings() {
-		PluginUtilities.saveSettings(this.getName(), this.settings);		
+		NeatoLib.Settings.save(this);		
 	}
-	
-	initialize() {
 
-		PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/NateUtilities.plugin.js");
+	onLibLoaded() {
+
+		NeatoLib.Updates.check(this);
 		
-		this.settings = PluginUtilities.loadSettings(this.getName(), {
+		this.settings = NeatoLib.Settings.load(this, {
             displayUpdateNotes : true,
             muteHotkey : "Alt + N",
             serverMuteHotkey : "Shift + Alt + N",
             serverDeafenHotkey : "Control + Alt + N"
 		});
 
-		let lib = document.getElementById("NeatoBurritoLibrary");
-		if(lib == undefined) {
-			lib = document.createElement("script");
-			lib.setAttribute("id", "NeatoBurritoLibrary");
-			lib.setAttribute("type", "text/javascript");
-			lib.setAttribute("src", "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js");
-			document.head.appendChild(lib);
-		}
-        if(typeof window.Metalloriff !== "undefined") this.onLibLoaded();
-		else lib.addEventListener("load", () => { this.onLibLoaded(); });
+        NeatoLib.Events.onPluginLoaded(this);
 		
-    }
-
-	onLibLoaded() {
-
-        Metalloriff.onPluginLoaded(this);
-		
-        //if(this.settings.displayUpdateNotes) Metalloriff.Changelog.compareVersions(this.getName(), this.getChanges());
+        //if(this.settings.displayUpdateNotes) NeatoLib.Changelog.compareVersions(this.getName(), this.getChanges());
         
         this.registerKeybinds();
 
@@ -114,60 +104,60 @@ class NateUtilities {
 
         if(!this.initialized) return;
 
-        this.selectedServer = PluginUtilities.getCurrentServer();
-        this.selectedVoiceChannel = Metalloriff.getSelectedVoiceChannel();
+        this.selectedServer = NeatoLib.getSelectedTextChannel();
+        this.selectedVoiceChannel = NeatoLib.getSelectedVoiceChannel();
 
     }
     
     registerKeybinds() {
 
         let nate = "209024642697003008",
-        toggleLocalMute = InternalUtilities.WebpackModules.findByUniqueProperties(["toggleLocalMute"]).toggleLocalMute,
-        isLocalMuted = InternalUtilities.WebpackModules.findByUniqueProperties(["isLocalMute"]).isLocalMute,
-        serverActionModule = InternalUtilities.WebpackModules.findByUniqueProperties(["setServerMute", "setServerDeaf"]),
-        getVoiceStates = InternalUtilities.WebpackModules.findByUniqueProperties(["getVoiceStates"]).getVoiceStates;
+        toggleLocalMute = NeatoLib.Modules.get("toggleLocalMute").toggleLocalMute,
+        isLocalMuted = NeatoLib.Modules.get("isLocalMute").isLocalMute,
+        serverActionModule = NeatoLib.Modules.get(["setServerMute", "setServerDeaf"]),
+        getVoiceStates = NeatoLib.Modules.get("getVoiceStates").getVoiceStates;
 
-        Metalloriff.Keybinds.registerGlobal(this.settings.muteHotkey, () => {
+        NeatoLib.Keybinds.registerGlobal(this.settings.muteHotkey, () => {
             toggleLocalMute(nate);
-            if(isLocalMuted(nate)) PluginUtilities.showToast("I got ya, fam!", { type : "success" });
-            else PluginUtilities.showToast("You're gonna regret that.", { type : "error" });
+            if(isLocalMuted(nate)) NeatoLib.showToast("I got ya, fam!", "success");
+            else NeatoLib.showToast("You're gonna regret that.", "error");
         });
 
-        Metalloriff.Keybinds.registerGlobal(this.settings.serverMuteHotkey, () => {
+        NeatoLib.Keybinds.registerGlobal(this.settings.serverMuteHotkey, () => {
             
             if(this.selectedServer && this.selectedVoiceChannel) {
 
                 let voiceStates = getVoiceStates(this.selectedServer.id);
 
                 if(voiceStates[nate] == undefined) {
-                    PluginUtilities.showToast("Nate is not here, you're safe!", { type : "success" });
+                    NeatoLib.showToast("Nate is not here, you're safe!", "success");
                     return;
                 }
 
                 serverActionModule.setServerMute(this.selectedServer.id, nate, !voiceStates[nate].mute);
 
-                if(!voiceStates[nate].mute) PluginUtilities.showToast("You should be proud!", { type : "success" });
-                else PluginUtilities.showToast("Sick fuck!", { type : "error" });
+                if(!voiceStates[nate].mute) NeatoLib.showToast("You should be proud!", "success");
+                else NeatoLib.showToast("Sick fuck!", "error");
 
             }
 
         });
 
-        Metalloriff.Keybinds.registerGlobal(this.settings.serverDeafenHotkey, () => {
+        NeatoLib.Keybinds.registerGlobal(this.settings.serverDeafenHotkey, () => {
 
             if(this.selectedServer && this.selectedVoiceChannel) {
 
                 let voiceStates = getVoiceStates(this.selectedServer.id);
 
                 if(voiceStates[nate] == undefined) {
-                    PluginUtilities.showToast("Nate is not here, you're safe!", { type : "success" });
+                    NeatoLib.showToast("Nate is not here, you're safe!", "success");
                     return;
                 }
 
                 serverActionModule.setServerDeaf(this.selectedServer.id, nate, !voiceStates[nate].deaf);
 
-                if(!voiceStates[nate].deaf) PluginUtilities.showToast("That Nate kid is an idiot.", { type : "success" });
-                else PluginUtilities.showToast("Shut up guys!", { type : "error" });
+                if(!voiceStates[nate].deaf) NeatoLib.showToast("That Nate kid is an idiot.", "success");
+                else NeatoLib.showToast("Shut up guys!", "error");
 
             }
 
@@ -176,9 +166,9 @@ class NateUtilities {
     }
 
     unregisterKeybinds() {
-        Metalloriff.Keybinds.unregisterGlobal(this.settings.muteHotkey);
-        Metalloriff.Keybinds.unregisterGlobal(this.settings.serverMuteHotkey);
-        Metalloriff.Keybinds.unregisterGlobal(this.settings.serverDeafenHotkey);
+        NeatoLib.Keybinds.unregisterGlobal(this.settings.muteHotkey);
+        NeatoLib.Keybinds.unregisterGlobal(this.settings.serverMuteHotkey);
+        NeatoLib.Keybinds.unregisterGlobal(this.settings.serverDeafenHotkey);
     }
 	
     stop() {

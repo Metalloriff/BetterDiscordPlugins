9c2c231d43300ad0efbe380b095998018fbd9232
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-06-11 13:51:39 -0400

    v1.0.3

--- SendLinksDirectly.plugin.js
+++ SendLinksDirectly.plugin.js
@@ -6,7 +6,7 @@ class SendLinksDirectly {
     getDescription() { return `Allows you to enclose direct links in square brackets to upload them directly, instead of sending a link.
     Usage: [link] or [link, filename.fileformat]
     Example: [https://static-cdn.jtvnw.net/emoticons/v1/521050/4.0, forsenE.png]`; }
-    getVersion() { return "0.0.3"; }
+    getVersion() { return "1.0.3"; }
 	getAuthor() { return "Metalloriff"; }
 	getChanges() {
 		return {
@@ -18,25 +18,10 @@ class SendLinksDirectly {
 
     start() {
 
-		var libraryScript = document.getElementById('zeresLibraryScript');
-		if (!libraryScript) {
-			libraryScript = document.createElement("script");
-			libraryScript.setAttribute("type", "text/javascript");
-			libraryScript.setAttribute("src", "https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js");
-			libraryScript.setAttribute("id", "zeresLibraryScript");
-			document.head.appendChild(libraryScript);
-		}
-		if (typeof window.ZeresLibrary !== "undefined") this.initialize();
-		else libraryScript.addEventListener("load", () => { this.initialize(); });
-
-	}
-	
-	initialize() {
-
-		PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/SendLinksDirectly.plugin.js");
-		
-        this.uploadFile = InternalUtilities.WebpackModules.findByUniqueProperties(["upload"]).upload;
-        this.hasPermission = InternalUtilities.WebpackModules.findByUniqueProperties(["can"]).can;
+        let libLoadedEvent = () => {
+            try{ this.onLibLoaded(); }
+            catch(err) { console.error(this.getName(), "fatal error, plugin could not be started!", err); }
+        };
 
 		let lib = document.getElementById("NeatoBurritoLibrary");
 		if(lib == undefined) {
@@ -46,80 +31,92 @@ class SendLinksDirectly {
 			lib.setAttribute("src", "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js");
 			document.head.appendChild(lib);
 		}
-        if(typeof window.Metalloriff !== "undefined") this.onLibLoaded();
-        else lib.addEventListener("load", () => { this.onLibLoaded(); });
-		
+        if(typeof window.NeatoLib !== "undefined") libLoadedEvent();
+        else lib.addEventListener("load", libLoadedEvent);
+
 	}
 
 	onLibLoaded() {
 
-        this.onSwitch();
+        NeatoLib.Updates.check(this);
+		
+        this.uploadFile = NeatoLib.Modules.get("upload").upload;
+        this.hasPermission = NeatoLib.Modules.get("can").can;
+        
+        NeatoLib.Events.onPluginLoaded(this);
 
-    }
-    
-    onSwitch() {
+        this.keyEvent = e => {
+            
+            let chatbox = e.target, selectedChannel = NeatoLib.getSelectedTextChannel();
 
-        var $chatbox = $(".chat textarea"), chatbox = $chatbox[0], selectedChannel = Metalloriff.getSelectedChannel();
-
-        if(selectedChannel == undefined) return;
-
-        if($chatbox.length) {
-
-            $chatbox.on("keydown.SendLinksDirectly", e => {
+            if(selectedChannel == undefined) return;
                 
-                if(e.which == 13 && !e.shiftKey && chatbox.value.trim() != "") {
+            if(e.which == 13 && !e.shiftKey && chatbox.value.trim() != "") {
 
-                    var search = chatbox.value.split("[").join("][").split("]"), links = new Array(), message = chatbox.value;
+                let search = chatbox.value.split("[").join("][").split("]"), links = [], message = chatbox.value;
 
-                    for(let i = 1; i < search.length - 1; i++) {
+                for(let i = 1; i < search.length - 1; i++) {
 
-                        if(search[i].startsWith("[http")) {
+                    if(search[i].startsWith("[http")) {
 
-                            var link = search[i].substring(1, search[i].length), args = link.split(",");
+                        let link = search[i].substring(1, search[i].length), args = link.split(",");
 
-                            links.push(args);
-                            
-                            message = message.replace(search[i] + "]", "");
-
-                        }
-
-                    }
-
-                    for(let i = 0; i < links.length; i++) {
-
-                        var filename = links[i][0].substring(links[i][0].lastIndexOf("/") + 1, links[i][0].length), urlVarStart = filename.lastIndexOf("?");
-
-                        if(urlVarStart != -1) filename = filename.substring(0, urlVarStart);
-
-                        Metalloriff.requestFile(links[i][0], links[i].length > 1 ? links[i][1] : filename, file => {
-
-                            this.uploadFile(selectedChannel.id, file, { content : i == 0 ? message : "", tts : false });
-
-                        });
-
-                    }
-
-                    if(links.length > 0) {
-
-                        chatbox.focus();
-                        chatbox.select();
-
-                        document.execCommand("insertText", false, "");
+                        links.push(args);
+                        
+                        message = message.replace(search[i] + "]", "");
 
                     }
 
                 }
 
-            });
+                for(let i = 0; i < links.length; i++) {
 
-        }
+                    let filename = links[i][0].substring(links[i][0].lastIndexOf("/") + 1, links[i][0].length), urlVarStart = filename.lastIndexOf("?");
+
+                    if(urlVarStart != -1) filename = filename.substring(0, urlVarStart);
+
+                    NeatoLib.requestFile(links[i][0], links[i].length > 1 ? links[i][1] : filename, file => {
+
+                        this.uploadFile(selectedChannel.id, file, { content : i == 0 ? message : "", tts : false });
+
+                    });
+
+                }
+
+                if(links.length > 0) {
+
+                    chatbox.focus();
+                    chatbox.select();
+
+                    document.execCommand("delete");
+
+                }
+
+            }
+
+        };
+
+        this.switchEvent = () => this.switch();
+
+        this.switch();
+
+        NeatoLib.Events.attach("switch", this.switchEvent);
+
+    }
+    
+    switch() {
+
+        let chatbox = NeatoLib.Chatbox.get();
+
+        if(chatbox == undefined) return;
+
+        chatbox.addEventListener("keydown", this.keyEvent);
 
     }
 	
     stop() {
-
-        $(".chat textarea").off("keydown.SendLinksDirectly");
-
+        if(NeatoLib.Chatbox.get()) NeatoLib.Chatbox.get().removeEventListener("keydown", this.keyEvent);
+        NeatoLib.Events.detach("switch", this.switchEvent);
 	}
 	
 }

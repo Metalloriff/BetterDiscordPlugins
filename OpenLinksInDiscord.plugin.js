f530b86ee79f24c2f66cc23d9b64da7e71e42e46
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-06-10 23:09:34 -0400

    v1.0.1

--- OpenLinksInDiscord.plugin.js
+++ OpenLinksInDiscord.plugin.js
@@ -4,37 +4,44 @@ class OpenLinksInDiscord {
 	
     getName() { return "OpenLinksInDiscord"; }
     getDescription() { return "Opens links in a new window in Discord, instead of in your web browser. Hold shift to open links normally."; }
-    getVersion() { return "0.0.1"; }
+    getVersion() { return "1.0.1"; }
 	getAuthor() { return "Metalloriff"; }
 
     load() {}
 
     start() {
 
-		var libraryScript = document.getElementById('zeresLibraryScript');
-		if (!libraryScript) {
-			libraryScript = document.createElement("script");
-			libraryScript.setAttribute("type", "text/javascript");
-			libraryScript.setAttribute("src", "https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js");
-			libraryScript.setAttribute("id", "zeresLibraryScript");
-			document.head.appendChild(libraryScript);
+        let libLoadedEvent = () => {
+            try{ this.onLibLoaded(); }
+            catch(err) { console.error(this.getName(), "fatal error, plugin could not be started!", err); }
+        };
+
+		let lib = document.getElementById("NeatoBurritoLibrary");
+		if(lib == undefined) {
+			lib = document.createElement("script");
+			lib.setAttribute("id", "NeatoBurritoLibrary");
+			lib.setAttribute("type", "text/javascript");
+			lib.setAttribute("src", "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js");
+			document.head.appendChild(lib);
 		}
-		if (typeof window.ZeresLibrary !== "undefined") this.initialize();
-		else libraryScript.addEventListener("load", () => { this.initialize(); });
+        if(typeof window.NeatoLib !== "undefined") libLoadedEvent();
+        else lib.addEventListener("load", libLoadedEvent);
 
 	}
 	
-	initialize() {
+	onLibLoaded() {
+		
+		NeatoLib.Updates.check(this);
 
-		PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/OpenLinksInDiscord.plugin.js");
-        
-        this.$document = $(document);
-
-        this.$document.on("click.OpenLinksInDiscord", e => {
+		this.event = e => {
             if(e.target.localName == "a" && e.target.href && e.target.href.startsWith("http") && !e.target.href.includes("/channels/") && !e.shiftKey) this.onClickLink(e);
-        });
+		};
+		
+		document.addEventListener("click", this.event);
 
 		this.electron = require("electron");
+
+		NeatoLib.Events.onPluginLoaded(this);
 		
 	}
     
@@ -51,7 +58,7 @@ class OpenLinksInDiscord {
     }
 	
     stop() {
-        this.$document.off("click.OpenLinksInDiscord");
+        document.removeEventListener("click", this.event);
 	}
 	
-} 
+}

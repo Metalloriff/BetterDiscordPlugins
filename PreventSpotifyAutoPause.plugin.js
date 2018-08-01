931864e19dc023bdfefca2c859fad1e9a19db319
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-07-31 20:26:37 -0400

    v0.0.1

---
+++ PreventSpotifyAutoPause.plugin.js
@@ -0,0 +1,47 @@
+//META{"name":"PreventSpotifyAutoPause","website":"https://metalloriff.github.io/toms-discord-stuff/","source":"https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/PreventSpotifyAutoPause.plugin.js"}*//
+
+class PreventSpotifyAutoPause {
+	
+    getName() { return "PreventSpotifyAutoPause"; }
+    getDescription() { return "Prevents Discord from automatically pausing Spotify after transmitting your microphone for 30 seconds."; }
+    getVersion() { return "0.0.1"; }
+	getAuthor() { return "Metalloriff"; }
+
+    load() {}
+
+    start() {
+
+        let libLoadedEvent = () => {
+            try{ this.onLibLoaded(); }
+            catch(err) { console.error(this.getName(), "fatal error, plugin could not be started!", err); try { this.stop(); } catch(err) { console.error(this.getName() + ".stop()", err); } }
+        };
+
+		let lib = document.getElementById("NeatoBurritoLibrary");
+		if(!lib) {
+			lib = document.createElement("script");
+			lib.id = "NeatoBurritoLibrary";
+			lib.type = "text/javascript";
+			lib.src = "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js";
+			document.head.appendChild(lib);
+		}
+		this.forceLoadTimeout = setTimeout(libLoadedEvent, 30000);
+        if(typeof window.NeatoLib !== "undefined") libLoadedEvent();
+		else lib.addEventListener("load", libLoadedEvent);
+
+	}
+
+	onLibLoaded() {
+		
+		NeatoLib.Updates.check(this);
+
+		this.unpatch = NeatoLib.monkeyPatchInternal(NeatoLib.Modules.get("pause"), "pause", () => {});
+		
+		NeatoLib.Events.onPluginLoaded(this);
+
+	}
+	
+    stop() {
+		this.unpatch();
+	}
+	
+}

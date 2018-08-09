1d145550eaa5f28d8227a89b35edf1f61c239b38
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-08-09 01:26:20 -0400

    v0.5.7

--- SaveTo.plugin.js
+++ SaveTo.plugin.js
@@ -1,14 +1,14 @@
 //META{"name":"SaveTo","website":"https://metalloriff.github.io/toms-discord-stuff/","source":"https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/SaveTo.plugin.js"}*//
 
 class SaveTo {
-	
+
 	getName() { return "Save To"; }
 	getDescription() { return "Allows you to save images, videos, files, server icons and user avatars to your defined folders, or browse to a folder, via the context menu."; }
-	getVersion() { return "0.5.6"; }
+	getVersion() { return "0.5.7"; }
 	getAuthor() { return "Metalloriff"; }
 	getChanges() {
 		return {
-			"0.1.2" : 
+			"0.1.2" :
 			`
 				Added sort mode settings, including custom sorting.
 				You can now add, remove and modify folders via the settings menu.
@@ -40,9 +40,13 @@ class SaveTo {
 		};
 	}
 
-	saveSettings() { NeatoLib.Settings.save(this); }
-	saveData() { NeatoLib.Data.save("SaveTo", "data", this.data); }
-	
+	saveSettings() {
+		NeatoLib.Settings.save(this);
+	}
+	saveData() {
+		NeatoLib.Data.save("SaveTo", "data", this.data);
+	}
+
 	getSettingsPanel() {
 
 		setTimeout(() => {
@@ -53,11 +57,11 @@ class SaveTo {
 
 				let fields = [];
 
-				for(let i = 0; i < this.data.folders.length; i++) {
+				for (let i = 0; i < this.data.folders.length; i++) {
 
 					let textField = NeatoLib.Settings.Elements.createNewTextField("", this.data.folders[i].path, e => {
 						let name = e.target.value.substring(e.target.value.split("\\").join("/").lastIndexOf("/") + 1, e.target.value.length);
-						if(e.target.value.trim().length == 0 || name.length == 0) {
+						if (e.target.value.trim().length == 0 || name.length == 0) {
 							this.data.folders.splice(i, 1);
 							e.target.parentElement.outerHTML = "";
 						} else {
@@ -75,11 +79,13 @@ class SaveTo {
 							this.data.folders[i].name = folder.name;
 							refreshFolders();
 							this.saveData();
-						}, { directory : true });
+						}, {
+							directory: true
+						});
 					}, "float:right;"));
 
 					let positionField = NeatoLib.Settings.Elements.createNewTextField("", this.data.folders[i].position, e => {
-						if(e.target.value.length == 0) e.target.value = this.data.folders[i].position;
+						if (e.target.value.length == 0) e.target.value = this.data.folders[i].position;
 						else this.data.folders[i].position = e.target.value;
 						refreshFolders();
 						this.saveData();
@@ -103,42 +109,71 @@ class SaveTo {
 
 				}
 
-				if(this.settings.sortMode == "a-z" || this.settings.sortMode == "z-a") fields = fields.sort((x, y) => {
-					if(x.getAttribute("data-name").toLowerCase() < y.getAttribute("data-name").toLowerCase()) return -1;
-					if(x.getAttribute("data-name").toLowerCase() > y.getAttribute("data-name").toLowerCase()) return 1;
+				if (this.settings.sortMode == "a-z" || this.settings.sortMode == "z-a") fields = fields.sort((x, y) => {
+					if (x.getAttribute("data-name").toLowerCase() < y.getAttribute("data-name").toLowerCase()) return -1;
+					if (x.getAttribute("data-name").toLowerCase() > y.getAttribute("data-name").toLowerCase()) return 1;
 					return 0;
 				});
 
-				if(this.settings.sortMode == "z-a" || this.settings.sortMode == "new-old") fields = fields.reverse();
+				if (this.settings.sortMode == "z-a" || this.settings.sortMode == "new-old") fields = fields.reverse();
 
-				if(this.settings.sortMode == "custom") fields = fields.sort((x, y) => {
-					if(parseFloat(x.getAttribute("data-priority")) > parseFloat(y.getAttribute("data-priority"))) return -1;
-					if(parseFloat(x.getAttribute("data-priority")) < parseFloat(y.getAttribute("data-priority"))) return 1;
+				if (this.settings.sortMode == "custom") fields = fields.sort((x, y) => {
+					if (parseFloat(x.getAttribute("data-priority")) > parseFloat(y.getAttribute("data-priority"))) return -1;
+					if (parseFloat(x.getAttribute("data-priority")) < parseFloat(y.getAttribute("data-priority"))) return 1;
 					return 0;
 				});
 
 				document.getElementById("st-folders").innerHTML = "";
 
-				for(let i = 0; i < fields.length; i++) document.getElementById("st-folders").insertAdjacentElement("beforeend", fields[i]);
+				for (let i = 0; i < fields.length; i++) document.getElementById("st-folders").insertAdjacentElement("beforeend", fields[i]);
 
 			};
 
-			NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createRadioGroup("st-file-name-type", "File name type:", [
-				{ title : "Original", value : "original", description : `Example: unknown.png` },
-				{ title : "Date", value : "date", description : `Example: ${date.toLocaleDateString().split("/").join("-")} ${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}.png` },
-				{ title : "Random", value : "random", description : `Example: ${Math.random().toString(36).substring(10)}.png` },
-				{ title : "Original + random", value : "original+random", description : `Example: unknown ${Math.random().toString(36).substring(10)}.png` }
+			NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createRadioGroup("st-file-name-type", "File name type:", [{
+					title: "Original",
+					value: "original",
+					description: `Example: unknown.png`
+				},
+				{
+					title: "Date",
+					value: "date",
+					description: `Example: ${date.toLocaleDateString().split("/").join("-")} ${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}.png`
+				},
+				{
+					title: "Random",
+					value: "random",
+					description: `Example: ${Math.random().toString(36).substring(10)}.png`
+				},
+				{
+					title: "Original + random",
+					value: "original+random",
+					description: `Example: unknown ${Math.random().toString(36).substring(10)}.png`
+				}
 			], this.settings.fileNameType, (e, choiceItem) => {
 				this.settings.fileNameType = choiceItem.value;
 				this.saveSettings();
 			}), this.getName());
 
-			NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createRadioGroup("st-sort-type", "Sort by:", [
-				{ title : "A - Z", value : "a-z" },
-				{ title : "Z - A", value : "z-a" },
-				{ title : "Newest - Oldest", value : "new-old" },
-				{ title : "Oldest - Newest", value : "old-new" },
-				{ title : "By priority", value : "custom" }
+			NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createRadioGroup("st-sort-type", "Sort by:", [{
+					title: "A - Z",
+					value: "a-z"
+				},
+				{
+					title: "Z - A",
+					value: "z-a"
+				},
+				{
+					title: "Newest - Oldest",
+					value: "new-old"
+				},
+				{
+					title: "Oldest - Newest",
+					value: "old-new"
+				},
+				{
+					title: "By priority",
+					value: "custom"
+				}
 			], this.settings.sortMode, (e, choiceItem) => {
 				this.settings.sortMode = choiceItem.value;
 				refreshFolders();
@@ -173,11 +208,15 @@ class SaveTo {
 				this.data.folders.splice(this.selectedFolder, 1);
 				refreshFolders();
 				this.saveData();
-			}, "margin-left:15px;", { id : "st-settings-remove-folder" }));
+			}, "margin-left:15px;", {
+				id: "st-settings-remove-folder"
+			}));
 
 			buttonParent.insertAdjacentElement("beforeend", NeatoLib.Settings.Elements.createButton("Open Selected Folder", () => {
 				window.open(`file:///${this.data.folders[this.selectedFolder].path}`);
-			}, "margin-left:15px;", { id : "st-settings-open-folder"}));
+			}, "margin-left:15px;", {
+				id: "st-settings-open-folder"
+			}));
 
 			NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createToggleSwitch("Move dropdown menu to the top of the context menu", this.settings.dropdownOnTop, () => {
 				this.settings.dropdownOnTop = !this.settings.dropdownOnTop;
@@ -195,147 +234,136 @@ class SaveTo {
 	load() {}
 
 	start() {
-
-		let libLoadedEvent = () => {
+		const libLoadedEvent = () => {
 			try{ this.onLibLoaded(); }
 			catch(err) { console.error(this.getName(), "fatal error, plugin could not be started!", err); try { this.stop(); } catch(err) { console.error(this.getName() + ".stop()", err); } }
 		};
 
 		let lib = document.getElementById("NeatoBurritoLibrary");
-		if(!lib) {
+		if (!lib) {
 			lib = document.createElement("script");
 			lib.id = "NeatoBurritoLibrary";
 			lib.type = "text/javascript";
 			lib.src = "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js";
 			document.head.appendChild(lib);
 		}
-		this.forceLoadTimeout = setTimeout(libLoadedEvent, 30000);
-		if(typeof window.NeatoLib !== "undefined") libLoadedEvent();
-		else lib.addEventListener("load", libLoadedEvent);
 
+		this.forceLoadTimeout = setTimeout(libLoadedEvent, 30000);
+		if (typeof window.NeatoLib !== "undefined") libLoadedEvent();
+		else lib.addEventListener("load", libLoadedEvent);
 	}
 
 	onLibLoaded() {
-
 		NeatoLib.Updates.check(this);
-		
+
 		this.data = NeatoLib.Data.load("SaveTo", "data", {
-			folders : []
+			folders: []
 		});
 
 		let updated = [];
 
-		for(let i = 0; i < this.data.folders.length; i++) {
-
-			if(typeof this.data.folders[i] !== "object") {
-
+		for (let i = 0; i < this.data.folders.length; i++) {
+			if (typeof this.data.folders[i] !== "object") {
 				let p = this.data.folders[i];
 
-				updated.push( {
-					path : p,
-					name : p.substring(p.split("\\").join("/").lastIndexOf("/") + 1, p.length),
-					position : i
+				updated.push({
+					path: p,
+					name: p.substring(p.split("\\").join("/").lastIndexOf("/") + 1, p.length),
+					position: i
 				});
-
 			}
-
 		}
 
-		if(updated.length > 0) this.data.folders = updated;
+		if (updated.length > 0) this.data.folders = updated;
 
 		this.settings = NeatoLib.Settings.load(this, {
-			fileNameType : "original",
-			sortMode : "a-z",
-			displayUpdateNotes : true,
-			dropdownOnTop : true
+			fileNameType: "original",
+			sortMode: "a-z",
+			displayUpdateNotes: true,
+			dropdownOnTop: true
 		});
 
 		this.classes = NeatoLib.getClasses(["contextMenu", "member"]);
 
-		this.contextMenuEvent = e => {
-			if(document.getElementsByClassName(this.classes.contextMenu).length == 0) setTimeout(() => this.onContextMenu(e), 0);
-			else this.onContextMenu(e);
-		};
-
 		this.selectedFolder = -1;
-		
-		document.addEventListener("contextmenu", this.contextMenuEvent);
+
+		document.addEventListener("contextmenu", this.contextMenuEvent = e => {
+			if (document.getElementsByClassName(this.classes.contextMenu).length == 0) setTimeout(() => this.onContextMenu(e), 0);
+			else this.onContextMenu(e);
+		});
 
 		NeatoLib.Changelog.compareVersions(this.getName(), this.getChanges());
 
+		NeatoLib.Events.onPluginLoaded(this);
 	}
 
 	onContextMenu(e) {
+		let member = NeatoLib.DOM.searchForParentElementByClassName(e.target, this.classes.member),
+			dm = NeatoLib.DOM.searchForParentElementByClassName(e.target, "private") || NeatoLib.DOM.searchForParentElementByClassName(e.target, "friends-row"),
+			messageGroup = NeatoLib.DOM.searchForParentElementByClassName(e.target, NeatoLib.getClass("containerCozy", "container"));
 
-		let member = NeatoLib.DOM.searchForParentElementByClassName(e.target, this.classes.member), dm = NeatoLib.DOM.searchForParentElementByClassName(e.target, "private") || NeatoLib.DOM.searchForParentElementByClassName(e.target, "friends-row"), messageGroup = NeatoLib.DOM.searchForParentElementByClassName(e.target, NeatoLib.getClass("containerCozy", "container"));
+		if (e.target.localName != "a" && e.target.localName != "img" && e.target.localName != "video" && !member && !dm && !messageGroup && !e.target.className.includes("guildIcon")) return;
 
-		if(e.target.localName != "a" && e.target.localName != "img" && e.target.localName != "video" && !member && !dm && !messageGroup && !e.target.className.includes("guildIcon")) return;
+		let saveLabel = "Save To",
+			url = e.target.poster || e.target.style.backgroundImage.substring(e.target.style.backgroundImage.indexOf(`"`) + 1, e.target.style.backgroundImage.lastIndexOf(`"`)) || e.target.href || e.target.src,
+			menu = [];
 
-		let saveLabel = "Save To", url = e.target.poster || e.target.style.backgroundImage.substring(e.target.style.backgroundImage.indexOf(`"`) + 1, e.target.style.backgroundImage.lastIndexOf(`"`)) || e.target.href || e.target.src, menu = [];
+		if (e.target.className.includes("guildIcon")) saveLabel = "Save Icon To";
 
-		if(e.target.className.includes("guildIcon")) saveLabel = "Save Icon To";
-
-		if(!url || e.target.className.includes("large")) {
-
-			if(messageGroup) {
-
+		if (!url || e.target.className.includes("large")) {
+			if (messageGroup) {
 				let user = NeatoLib.ReactData.getProp(messageGroup, "messages.0.author");
 
 				url = user.getAvatarURL();
-				if(user.avatar.startsWith("a_")) url = url.replace(".png", ".gif");
+				if (user.avatar.startsWith("a_")) url = url.replace(".png", ".gif");
 
 				saveLabel = "Save Avatar To";
-
 			}
 
-			if(member) {
-
+			if (member) {
 				let user = NeatoLib.ReactData.getProps(member).user;
 
 				url = user.getAvatarURL();
-				if(user.avatar.startsWith("a_")) url = url.replace(".png", ".gif");
+				if (user.avatar.startsWith("a_")) url = url.replace(".png", ".gif");
 
 				saveLabel = "Save Avatar To";
-
 			}
 
-			if(dm) {
-
+			if (dm) {
 				let unparsedUrl = dm.getElementsByClassName("avatar-small")[0].style.backgroundImage;
 
 				url = unparsedUrl.substring(unparsedUrl.indexOf(`"`) + 1, unparsedUrl.lastIndexOf(`"`));
 
-				if(url.includes("a_")) url = url.replace(".png", ".gif");
+				if (url.includes("a_")) url = url.replace(".png", ".gif");
 
 				saveLabel = "Save Avatar To";
-
 			}
-
 		}
 
-		if(!url || e.target.classList.contains("emote") || url.includes("youtube.com/")) return;
+		if (!url || e.target.classList.contains("emote") || url.includes("youtube.com/")) return;
 
 		url = url.split("?")[0];
 
-		if(saveLabel.includes("Avatar") || saveLabel.includes("Icon")) url += "?size=2048";
+		if (saveLabel.includes("Avatar") || saveLabel.includes("Icon")) url += "?size=2048";
 
 		url = url.replace(".webp", ".png");
 
-		let fileName = url.substring(url.lastIndexOf("/") + 1, url.length), fileExtension = url.substring(url.lastIndexOf("."), url.length);
+		let fileName = url.substring(url.lastIndexOf("/") + 1, url.length),
+			fileExtension = url.substring(url.lastIndexOf("."), url.length);
 
-		if(e.target.classList.contains("emoji")) {
+		if (e.target.classList.contains("emoji")) {
 			saveLabel = "Save Emote To";
 			fileName = NeatoLib.ReactData.getProps(e.target).emojiName.replace(/[^A-Za-z]/g, "") + fileExtension;
 		}
-			
+
 		let date = new Date();
 
-		if(this.settings.fileNameType == "date") fileName = `${date.toLocaleDateString().split("/").join("-")} ${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}${fileExtension}`;
+		if (this.settings.fileNameType == "date") fileName = `${date.toLocaleDateString().split("/").join("-")} ${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}${fileExtension}`;
 
-		if(this.settings.fileNameType == "random") fileName = `${Math.random().toString(36).substring(10)}${fileExtension}`;
+		if (this.settings.fileNameType == "random") fileName = `${Math.random().toString(36).substring(10)}${fileExtension}`;
 
-		if(this.settings.fileNameType == "original+random") fileName = fileName.replace(fileExtension, "") + ` ${Math.random().toString(36).substring(10)}${fileExtension}`;
-		
+		if (this.settings.fileNameType == "original+random") fileName = fileName.replace(fileExtension, "") + ` ${Math.random().toString(36).substring(10)}${fileExtension}`;
+
 		let optionsSubMenu = i => {
 			let r = [];
 			r.push(NeatoLib.ContextMenu.createItem("Remove Folder", e => {
@@ -357,60 +385,62 @@ class SaveTo {
 
 		let sorted = this.data.folders;
 
-		if(this.settings.sortMode == "a-z" || this.settings.sortMode == "z-a") sorted = sorted.sort((x, y) => {
-			if(x.name.toLowerCase() < y.name.toLowerCase()) return -1;
-			if(x.name.toLowerCase() > y.name.toLowerCase()) return 1;
+		if (this.settings.sortMode == "a-z" || this.settings.sortMode == "z-a") sorted = sorted.sort((x, y) => {
+			if (x.name.toLowerCase() < y.name.toLowerCase()) return -1;
+			if (x.name.toLowerCase() > y.name.toLowerCase()) return 1;
 			return 0;
 		});
 
-		if(this.settings.sortMode == "z-a" || this.settings.sortMode == "old-new") sorted = sorted.reverse();
+		if (this.settings.sortMode == "z-a" || this.settings.sortMode == "old-new") sorted = sorted.reverse();
 
-		if(this.settings.sortMode == "custom") sorted = sorted.sort((x, y) => {
-			if(x.position > y.position) return -1;
-			if(x.position < y.position) return 1;
+		if (this.settings.sortMode == "custom") sorted = sorted.sort((x, y) => {
+			if (x.position > y.position) return -1;
+			if (x.position < y.position) return 1;
 			return 0;
 		});
 
 		let g = [];
-		for(let i = 0; i < this.data.folders.length; i++) {
-			g.push(NeatoLib.ContextMenu.createSubMenu(this.data.folders[i].name, optionsSubMenu(i), { callback : () => NeatoLib.downloadFile(url, this.data.folders[i].path, fileName) }));
+		for (let i = 0; i < this.data.folders.length; i++) {
+			g.push(NeatoLib.ContextMenu.createSubMenu(this.data.folders[i].name, optionsSubMenu(i), {
+				callback: () => NeatoLib.downloadFile(url, this.data.folders[i].path, fileName)
+			}));
 		}
 		menu.push(NeatoLib.ContextMenu.createGroup(g));
 
 		menu.push(NeatoLib.ContextMenu.createGroup([
 			NeatoLib.ContextMenu.createItem("Add Folder", () => this.browseForFolder),
-			NeatoLib.ContextMenu.createItem("Browse", () => NeatoLib.browseForFile(folder => NeatoLib.downloadFile(url, folder.path, fileName), { directory : true })),
-			NeatoLib.ContextMenu.createItem("Plugin Settings", () => { NeatoLib.Settings.showPluginSettings(this.getName()); NeatoLib.ContextMenu.close(); })
+			NeatoLib.ContextMenu.createItem("Browse", () => NeatoLib.browseForFile(folder => NeatoLib.downloadFile(url, folder.path, fileName), {
+				directory: true
+			})),
+			NeatoLib.ContextMenu.createItem("Plugin Settings", () => {
+				NeatoLib.Settings.showPluginSettings(this.getName());
+				NeatoLib.ContextMenu.close();
+			})
 		]));
 
-		if(NeatoLib.ContextMenu.get()) NeatoLib.ContextMenu.get().insertAdjacentElement(this.settings.dropdownOnTop ? "afterbegin" : "beforeend", NeatoLib.ContextMenu.createGroup([NeatoLib.ContextMenu.createSubMenu(saveLabel, menu)]));
-
+		if (NeatoLib.ContextMenu.get()) NeatoLib.ContextMenu.get().insertAdjacentElement(this.settings.dropdownOnTop ? "afterbegin" : "beforeend", NeatoLib.ContextMenu.createGroup([NeatoLib.ContextMenu.createSubMenu(saveLabel, menu)]));
 	}
 
 	browseForFolder(selected) {
-
 		NeatoLib.browseForFile(folder => {
-
-			if(this.data.folders.findIndex(f => f.path == folder.path) == -1) {
-
+			if (this.data.folders.findIndex(f => f.path == folder.path) == -1) {
 				this.data.folders.push({
-					path : folder.path,
-					name : folder.name,
-					position : this.data.folders.length
+					path: folder.path,
+					name: folder.name,
+					position: this.data.folders.length
 				});
 
 				this.saveData();
-
 			}
 
-			if(selected) selected();
-
-		}, { directory : true });
-
+			if (selected) selected();
+		}, {
+			directory: true
+		});
 	}
-	
+
 	stop() {
 		document.removeEventListener("contextmenu", this.contextMenuEvent);
 	}
-	
+
 }

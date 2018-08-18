a573619c63503c970d75ffc63bd332ce1eb6c366
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2018-08-17 23:38:55 -0400

    v1.2.7

--- ViewGuildRelationships.plugin.js
+++ ViewGuildRelationships.plugin.js
@@ -2,10 +2,10 @@
 
 class ViewGuildRelationships {
 
-  getName() { return "View Guild Relationships"; }
-  getDescription() { return "Adds a 'View Relationships' button to the guild dropdown and context menu that opens a list of all friends, requested friends, and blocked users in the server."; }
-  getVersion() { return "1.1.7"; }
-  getAuthor() { return "Metalloriff"; }
+	getName() { return "View Guild Relationships"; }
+	getDescription() { return "Adds a 'View Relationships' button to the guild dropdown and context menu that opens a list of all friends, requested friends, and blocked users in the server."; }
+	getVersion() { return "1.2.7"; }
+	getAuthor() { return "Metalloriff"; }
 
 	load() {}
 
@@ -35,7 +35,6 @@ class ViewGuildRelationships {
 		NeatoLib.Updates.check(this);
 
 		this.style = NeatoLib.injectCSS(`
-
 			.vgr-separator {
 				font-weight: bold;
 				color: white;
@@ -43,6 +42,9 @@ class ViewGuildRelationships {
 				padding: 10px;
 				margin: 10px;
 				border-radius: 5px;
+			}
+
+			.vgr-separator > :first-child {
 				cursor: pointer;
 			}
 
@@ -72,10 +74,9 @@ class ViewGuildRelationships {
 				border-radius: 5px;
 				transition: all 0.3s;
 				color: lightgray;
-				cursor: default;
 			}
 
-			.vgr-separator.collapsed > * {
+			.vgr-separator.collapsed > :not(:first-child) {
 				display: none;
 			}
 
@@ -95,6 +96,28 @@ class ViewGuildRelationships {
 				bottom: 20px;
 			}
 
+			.vgr-mutual-servers {
+				float: right;
+			}
+
+			.vgr-mutual-servers > .vgr-friend-item-avatar {
+				display: inline-block;
+				margin-left: 10px;
+				width: 48px;
+				height: 48px;
+			}
+
+			.vgr-mutual-servers > .vgr-more {
+				text-align: center;
+				vertical-align: top;
+				line-height: 47px;
+				font-size: 20px;
+				color: white !important;
+			}
+
+			.vgr-mutual-servers > :not(.vgr-more) {
+				cursor: pointer;
+			}
 		`);
 
 		this.contextMenuEvent = e => this.onContextMenu(e);
@@ -119,6 +142,18 @@ class ViewGuildRelationships {
 		}
 	}
 
+	getMutualServers(uid) {
+		const guilds = NeatoLib.Modules.get("getGuilds").getGuilds(), isMember = NeatoLib.Modules.get("isMember").isMember, mutual = [];
+
+		for (let gid in guilds) {
+			if (isMember(gid, uid)) {
+				mutual.push(guilds[gid]);
+			}
+		}
+
+		return mutual;
+	}
+
 	getRelationships(gid) {
 		NeatoLib.ContextMenu.close();
 
@@ -128,23 +163,23 @@ class ViewGuildRelationships {
 		let win = NeatoLib.UI.createBasicScrollList("vgr-relationshipswindow", "Relationships in " + guild.name);
 
 		win.scroller.insertAdjacentHTML("beforeend", `
-			<div id="vgr-friends-separator" onclick="if(this.classList.contains('collapsed')) this.classList.remove('collapsed'); else this.classList.add('collapsed');" class="vgr-separator">Friends<div class="vgr-nonefound">No friends found here.</div></div>
+			<div id="vgr-friends-separator" class="vgr-separator"><div onclick="if(this.parentElement.classList.contains('collapsed')) this.parentElement.classList.remove('collapsed'); else this.parentElement.classList.add('collapsed');">Friends</div><div class="vgr-nonefound">No friends found here.</div></div>
 
-			<div id="vgr-blocked-separator" onclick="if(this.classList.contains('collapsed')) this.classList.remove('collapsed'); else this.classList.add('collapsed');" class="vgr-separator">Blocked Users<div class="vgr-nonefound">No blocked users found here.</div></div>
+			<div id="vgr-blocked-separator" class="vgr-separator"><div onclick="if(this.parentElement.classList.contains('collapsed')) this.parentElement.classList.remove('collapsed'); else this.parentElement.classList.add('collapsed');">Blocked Users</div><div class="vgr-nonefound">No blocked users found here.</div></div>
 
-			<div id="vgr-incoming-separator" onclick="if(this.classList.contains('collapsed')) this.classList.remove('collapsed'); else this.classList.add('collapsed');" class="vgr-separator">Incoming Friend Requests<div class="vgr-nonefound">No incoming friend requests found here.</div></div>
+			<div id="vgr-incoming-separator" class="vgr-separator"><div onclick="if(this.parentElement.classList.contains('collapsed')) this.parentElement.classList.remove('collapsed'); else this.parentElement.classList.add('collapsed');">Incoming Friend Requests</div><div class="vgr-nonefound">No incoming friend requests found here.</div></div>
 
-			<div id="vgr-outgoing-separator" onclick="if(this.classList.contains('collapsed')) this.classList.remove('collapsed'); else this.classList.add('collapsed');" class="vgr-separator">Outgoing Friend Requests<div class="vgr-nonefound">No outgoing friend requests found here.</div></div>
+			<div id="vgr-outgoing-separator" class="vgr-separator"><div onclick="if(this.parentElement.classList.contains('collapsed')) this.parentElement.classList.remove('collapsed'); else this.parentElement.classList.add('collapsed');">Outgoing Friend Requests</div><div class="vgr-nonefound">No outgoing friend requests found here.</div></div>
 		`);
 
 		let friends = document.getElementById("vgr-friends-separator"),
-			noFriendsFound = friends.firstElementChild,
+			noFriendsFound = friends.getElementsByClassName("vgr-nonefound")[0],
 			blocked = document.getElementById("vgr-blocked-separator"),
-			noBlockedFound = blocked.firstElementChild,
+			noBlockedFound = blocked.getElementsByClassName("vgr-nonefound")[0],
 			incoming = document.getElementById("vgr-incoming-separator"),
-			noIncomingFound = incoming.firstElementChild,
+			noIncomingFound = incoming.getElementsByClassName("vgr-nonefound")[0],
 			outgoing = document.getElementById("vgr-outgoing-separator"),
-			noOutgoingFound = outgoing.firstElementChild;
+			noOutgoingFound = outgoing.getElementsByClassName("vgr-nonefound")[0];
 
 		let friendItem = user => {
 			return `
@@ -153,6 +188,34 @@ class ViewGuildRelationships {
 					<div class="vgr-friend-item-username">${user.username}</div>
 				</div>
 			`;
+		};
+
+		let mutualServerList = user => {
+			const mutual = this.getMutualServers(user.id), element = document.createElement("div"), overflow = [];
+
+			element.className = "vgr-mutual-servers";
+
+			for (let i = 0; i < mutual.length; i++) {
+				if (i > 4) {
+					overflow.push(mutual[i].name);
+					continue;
+				}
+
+				element.insertAdjacentHTML("afterBegin", `<img class="vgr-friend-item-avatar" src="${mutual[i].getIconURL()}" height="48px" width="48px">`);
+				NeatoLib.Tooltip.attach(mutual[i].name, element.firstElementChild);
+
+				element.firstElementChild.addEventListener("click", () => {
+					NeatoLib.Modules.get("selectGuild").selectGuild(mutual[i].id);
+					win.window.remove();
+				});
+			}
+
+			if (overflow.length) {
+				element.insertAdjacentHTML("afterBegin", `<div class="vgr-friend-item-avatar vgr-more">+${overflow.length}</div>`);
+				NeatoLib.Tooltip.attach(overflow.join(", "), element.firstElementChild);
+			}
+
+			return element;
 		};
 
 		for (let uid in relationships) {
@@ -166,24 +229,28 @@ class ViewGuildRelationships {
 				if (noFriendsFound) noFriendsFound.remove();
 				friends.insertAdjacentHTML("beforeend", friendItem(user));
 				added = friends.lastElementChild;
+				added.appendChild(mutualServerList(user));
 			}
 
 			if (relationships[uid] == 2) {
 				if (noBlockedFound) noBlockedFound.remove();
 				blocked.insertAdjacentHTML("beforeend", friendItem(user));
 				added = blocked.lastElementChild;
+				added.appendChild(mutualServerList(user));
 			}
 
 			if (relationships[uid] == 3) {
 				if (noIncomingFound) noIncomingFound.remove();
 				incoming.insertAdjacentHTML("beforeend", friendItem(user));
 				added = incoming.lastElementChild;
+				added.appendChild(mutualServerList(user));
 			}
 
 			if (relationships[uid] == 4) {
 				if (noOutgoingFound) noOutgoingFound.remove();
 				outgoing.insertAdjacentHTML("beforeend", friendItem(user));
 				added = outgoing.lastElementChild;
+				added.appendChild(mutualServerList(user));
 			}
 
 			if (added) {

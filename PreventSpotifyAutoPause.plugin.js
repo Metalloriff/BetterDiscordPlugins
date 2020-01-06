4b7690973025a2b392d6c1eb22db4297c6e612a4
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2020-01-06 00:21:07 -0500

    No longer shows the notification

--- PreventSpotifyAutoPause.plugin.js
+++ PreventSpotifyAutoPause.plugin.js
@@ -1,43 +1,97 @@
-//META{"name":"PreventSpotifyAutoPause","website":"https://metalloriff.github.io/toms-discord-stuff/","source":"https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/PreventSpotifyAutoPause.plugin.js"}*//
-
-class PreventSpotifyAutoPause {
+//META{"name":"PreventSpotifyAutoPause","displayName":"PreventSpotifyAutoPause","website":"https://metalloriff.github.io/toms-discord-stuff/","source":"https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/PreventSpotifyAutoPause.plugin.js"}*//
+/*@cc_on
+@if (@_jscript)
 	
-    getName() { return "PreventSpotifyAutoPause"; }
-    getDescription() { return "Prevents Discord from automatically pausing Spotify after transmitting your microphone for 30 seconds."; }
-    getVersion() { return "0.0.2"; }
-	getAuthor() { return "Metalloriff"; }
-
-    load() {}
-
-    start() {
-        const libLoadedEvent = () => {
-            try{ this.onLibLoaded(); }
-            catch(err) { console.error(this.getName(), "fatal error, plugin could not be started!", err); try { this.stop(); } catch(err) { console.error(this.getName() + ".stop()", err); } }
-        };
-
-		let lib = document.getElementById("NeatoBurritoLibrary");
-		if(!lib) {
-			lib = document.createElement("script");
-			lib.id = "NeatoBurritoLibrary";
-			lib.type = "text/javascript";
-			lib.src = "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js";
-			document.head.appendChild(lib);
-		}
-		this.forceLoadTimeout = setTimeout(libLoadedEvent, 30000);
-        if(typeof window.NeatoLib !== "undefined") libLoadedEvent();
-		else lib.addEventListener("load", libLoadedEvent);
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
 	}
+	WScript.Quit();
+@else@*/
 
-	onLibLoaded() {
-		NeatoLib.Updates.check(this);
+var PreventSpotifyAutoPause = (() => {
+	const config = {
+		info: {
+			name: "PreventSpotifyAutoPause",
+			authors: [{
+				name: "Metalloriff",
+				discord_id: "264163473179672576",
+				github_username: "Metalloriff",
+				twitter_username: "Metalloriff"
+			}],
+			version: "1.0.2",
+			description: "Prevents Discord from automatically pausing Spotify after transmitting your microphone for 30 seconds.",
+			github: "https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/PreventSpotifyAutoPause.plugin.js",
+			github_raw: "https://raw.githubusercontent.com/Metalloriff/BetterDiscordPlugins/master/PreventSpotifyAutoPause.plugin.js"
+		},
+		changelog: [{
+			title: "Rewrite",
+			type: "fixed",
+			items: ["Rewritten with new libraries."]
+		}],
+		main: "index.js",
+		defaultConfig: []
+	};
 
-		this.unpatch = NeatoLib.monkeyPatchInternal(NeatoLib.Modules.get(["SpotifyResourceTypes", "pause"]), "pause", () => {});
+	return !global.ZeresPluginLibrary || !global.XenoLib ? class {
+		getName() { return config.info.name; }
+		getAuthor() { return config.info.authors.map(x => x.name).join(", "); }
+		getDescription() { return config.info.description; }
+		getVersion() { return config.info.version; }
 		
-		NeatoLib.Events.onPluginLoaded(this);
-	}
+		load() {
+            const title = "Library Missing";
+            const ModalStack = BdApi.findModuleByProps("push", "update", "pop", "popWithKey");
+            const TextElement = BdApi.findModuleByProps("Sizes", "Weights");
+            const ConfirmationModal = BdApi.findModule(m => m.defaultProps && m.key && m.key() == "confirm-modal");
+            if (!ModalStack || !ConfirmationModal || !TextElement) return BdApi.alert(title, `The library plugin needed for ${config.info.name} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
+            ModalStack.push(function(props) {
+                return BdApi.React.createElement(ConfirmationModal, Object.assign({
+                    header: title,
+                    children: [TextElement({color: TextElement.Colors.PRIMARY, children: [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`]})],
+                    red: false,
+                    confirmText: "Download Now",
+                    cancelText: "Cancel",
+                    onConfirm: () => {
+                        require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                            if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                            await new Promise(r => require("fs").writeFile(require("path").join(ContentManager.pluginsFolder, "0PluginLibrary.plugin.js"), body, r));
+                        });
+                    }
+                }, props));
+            });
+        }
+  
+		
+		start() {}
+		stop() {}
+	} : (([Plugin, Api]) => {
+		const plugin = (Plugin, Api) => {
+			return class PreventSpotifyAutoPause extends Plugin {
+				onStart() {
+                    this.unpatch = Api.Patcher.instead(ZLibrary.WebpackModules.getByProps("SpotifyResourceTypes", "pause"), "pause", () => {});
+				}
 	
-    stop() {
-		this.unpatch();
-	}
-	
-}
+				onStop() {
+                    this.unpatch();
+				}
+			}
+		};
+
+		return plugin(Plugin, Api);
+	})(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/

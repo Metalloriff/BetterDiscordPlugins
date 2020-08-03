b6e44f0be89c3f96cfa4d2fe2635e3c9a064449c
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2020-08-03 14:32:30 -0400

    fuck my trash ass lib

--- TheClapBestClapPluginClapEver.plugin.js
+++ TheClapBestClapPluginClapEver.plugin.js
@@ -1,130 +1,126 @@
-//META{"name":"TheClapBestClapPluginClapEver","website":"https://metalloriff.github.io/toms-discord-stuff/","source":"https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/TheClapBestClapPluginClapEver.plugin.js"}*//
+/**
+ * @name TheClapBestClapPluginClapEver
+ * @invite yNqzuJa
+ * @authorLink https://github.com/Metalloriff
+ * @donate https://www.paypal.me/israelboone
+ * @website https://metalloriff.github.io/toms-discord-stuff/
+ * @source https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/TheClapBestClapPluginClapEver.plugin.js
+ */
 
-class TheClapBestClapPluginClapEver {
-	
-    getName() { return "The Clap Best Clap Plugin Clap Ever"; }
-	getDescription() { return `Literally the most useless and cancerous plugin ever. Put "clapclap:" at the first of your message to replace spaces with clap emojis. You can also do "clapclap(some_emote_name):" to use custom emotes, "superclapclap" for every character instead of every space, "ra:" to replace all characters with regional indicators, "reverse:" to reverse the message, "b:" for the good shit, and "owo:" for complete fucking cancer.`; }
-    getVersion() { return "0.4.7"; }
-    getAuthor() { return "Metalloriff"; }
-
-    load() {}
-
-    start() {
-
-        let libLoadedEvent = () => {
-            try{ this.onLibLoaded(); }
-            catch(err) { console.error(this.getName(), "fatal error, plugin could not be started!", err); try { this.stop(); } catch(err) { console.error(this.getName() + ".stop()", err); } }
-        };
-
-		let lib = document.getElementById("NeatoBurritoLibrary");
-		if(!lib) {
-			lib = document.createElement("script");
-			lib.id = "NeatoBurritoLibrary";
-			lib.type = "text/javascript";
-			lib.src = "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js";
-			document.head.appendChild(lib);
+module.exports = (() =>
+{
+    const config =
+    {
+		info:
+		{
+			name: "TheClapBestClapPluginClapEver",
+			authors:
+			[
+				{
+					name: "Metalloriff",
+					discord_id: "264163473179672576",
+					github_username: "metalloriff",
+					twitter_username: "Metalloriff"
+				}
+			],
+			version: "2.0.0",
+			description: "Literally useless, toxic cancer. Write 'clapclap$' at the beginning of your message to separate each word with a clap emoji. Write 'clapclap( :your_emoji: )$' to separate each word with your own custom emoji. 'superclapclap$' or 'superclapclap( :emoji: )$' for every letter instead of every word. 'ra$' to replace every letter with a regional indicator emoji. 'reverse$' to reverse your message. 'b$' to replace every 'b' with the B emoji. 'owo$' if you have no will to live.",
+			github: "https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/TheClapBestClapPluginClapEver.plugin.js",
+			github_raw: "https://raw.githubusercontent.com/Metalloriff/BetterDiscordPlugins/master/TheClapBestClapPluginClapEver.plugin.js"
 		}
-		this.forceLoadTimeout = setTimeout(libLoadedEvent, 30000);
-        if(typeof window.NeatoLib !== "undefined") libLoadedEvent();
-		else lib.addEventListener("load", libLoadedEvent);
+    };
 
-	}
+    return (([Plugin, Api]) => {
+
+		const plugin = (Plugin, Api) =>
+		{
+			const { DiscordModules, Patcher } = Api;
+
+			return class TheClapBestClapPluginClapEver extends Plugin
+			{
+				constructor()
+				{
+					super();
+				}
 	
-	onLibLoaded() {
+				onStart()
+				{
+					Patcher.after(DiscordModules.MessageActions, "sendMessage", (_, [, message]) =>
+					{
+						const content = message.content.toLowerCase();
+						const clapclap = (/^clapclap(\S| )*\$ /g).exec(content) || (/^superclapclap(\S| )*\$ /g).exec(content);
+						
+						if (clapclap)
+						{	
+							const filler = clapclap[0].includes("(") && clapclap[0].includes(")")
+								? clapclap[0].substr(clapclap[0].indexOf("(") + 1, clapclap[0].indexOf(")") - clapclap[0].indexOf("(") - 1)
+								: " :clap: ";
+								
+							message.content = message.content.substr(clapclap[0].length, message.content.length)
+								.split(clapclap[0].startsWith("super") ? "" : " ")
+								.join(filler);
+								
+							message.content = filler + message.content + filler;
 
-		NeatoLib.Updates.check(this);
+							return;
+						}
 
-		this.onChatInput = e => {
+						switch (content.split("$")[0])
+						{
+							case "ra":
+								const ra = (/^ra\$ /g).exec(content);
 
-			const chatbox = e.target;
+								message.content = message.content.substr(ra[0].length, message.content.length)
+									.split(" ")
+									.join("\t")
+									.replace(/[A-Za-z]/g, x => ` :regional_indicator_${x}: `);
+								
+								break;
 
-			if(e.which == 13 && !e.shiftKey && chatbox.value){
+							case "reverse":
+								const reverse = (/^reverse\$ /g).exec(content);
 
-				let chatboxValue = chatbox.value.trim(), changed = false;
+								message.content = message.content.substr(reverse[0].length, message.content.length)
+									.split("")
+									.reverse()
+									.join("");
 
-				if(chatboxValue.startsWith("clapclap") || chatboxValue.startsWith("superclapclap")){
+								break;
 
-					let getClapClap = chatboxValue.substring(0, chatboxValue.indexOf(":") + 1), emote = "clap", definedEmote = getClapClap.substring(getClapClap.indexOf("(") + 1, getClapClap.indexOf(")"));
+							case "owo":
+								const owo = (/^owo\$ /g).exec(content);
 
-					if(definedEmote) emote = definedEmote;
+								message.content = message.content.substr(owo[0].length, message.content.length)
+									.replace(/r/g, "w")
+									.replace(/R/g, "W")
+									.replace(/l/g, "w")
+									.replace(/L/g, "W")
+									.replace(/ n/g, " ny")
+									.replace(/ N/g, " Ny")
+									.replace(/ove/g, "uv")
+									.replace(/OVE/g, "UV")
+									+ " " + ["owo", "OwO", "uwu", "UwU", ">w<", "^w^", "♥w♥"][7 * Math.random() << 0];
 
-					emote = " :" + emote  + ": ";
+								break;
 
-					chatboxValue = chatboxValue.replace(getClapClap, "").split(chatboxValue.startsWith("super") ? "" : " ").join(emote);
+							case "b":
+								const b = (/^b\$ /g).exec(content);
 
-					if(chatboxValue.startsWith(emote)) chatboxValue += emote;
+								message.content = message.content.substr(b[0].length, message.content.length)
+									.replace(/b/g, ":b:");
 
-					changed = true;
-					
+								break;
+						}
+					});
 				}
-
-				if(chatboxValue.startsWith("ra:")){
-
-					let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", temp = chatboxValue.split(" ").join("\t");
-
-					for(let i = 0; i < alphabet.length; i++) temp = temp.replace("ra:", "").split(alphabet[i]).join("[a" + i + "]");
-					for(let i = 0; i < alphabet.length; i++) temp = temp.split("[a" + i + "]").join(":regional_indicator_" + alphabet[i].toLowerCase() + ": ");
-
-					temp = temp.split("?").join(":question:");
-
-					chatboxValue = temp;
-
-					changed = true;
-
+	
+				onStop()
+				{
+					Patcher.unpatchAll();
 				}
-
-				if(chatboxValue.startsWith("reverse:")) {
-					chatboxValue = chatboxValue.replace("reverse:", "").split("").reverse().join("");
-					changed = true;
-				}
-
-				if(chatboxValue.startsWith("owo:")) {
-
-					const cancer = ["owo", "OwO", "uwu", "UwU", ">w<", "^w^"];
-
-					chatboxValue = chatboxValue.replace("owo:", "").trim();
-
-					chatboxValue = chatboxValue.split("r").join("w").split("R").join("W").split("l").join("w").split("L").join("W").split(" n").join(" ny").split(" N").join(" Ny").split("ove").join("uv").split("OVE").join("UV");
-
-					chatboxValue += " " + cancer[cancer.length * Math.random() << 0];
-
-					if(chatbox.value == chatbox.value.toUpperCase()) chatboxValue = chatboxValue.toUpperCase();
-
-					changed = true;
-
-				}
-
-				if(chatboxValue.toLowerCase().startsWith("b:")) {
-					chatboxValue = chatboxValue.substring(2, chatboxValue.length).split("b").join(":b:");
-					changed = true;
-				}
-				
-				if(changed) {
-					chatbox.select();
-					document.execCommand("insertText", false, chatboxValue);
-				}
-
 			}
-
 		};
 
-		this.switch();
-
-		NeatoLib.Events.attach("switch", this.switchEvent = () => this.switch());
-
-		NeatoLib.Events.onPluginLoaded(this);
-
-	}
-	
-	switch() {
-		const chatbox = NeatoLib.Chatbox.get();
-		if(chatbox) chatbox.addEventListener("keydown", this.onChatInput);
-	}
-	
-    stop() {
-		const chatbox = NeatoLib.Chatbox.get();
-		if(chatbox) chatbox.removeEventListener("keydown", this.onChatInput);
-		NeatoLib.Events.detach("switch", this.switchEvent);
-	}
-	
-}
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
+})();

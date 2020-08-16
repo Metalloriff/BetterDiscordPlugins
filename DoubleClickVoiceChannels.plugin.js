d2ab576f28e6018f0aaee8ed26320dad630788f3
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2020-08-16 17:08:16 -0400

    fuck discord

--- DoubleClickVoiceChannels.plugin.js
+++ DoubleClickVoiceChannels.plugin.js
@@ -23,11 +23,19 @@ module.exports = (() =>
 					twitter_username: "Metalloriff"
 				}
 			],
-			version: "2.0.0",
+			version: "2.0.1",
 			description: "Requires you to double click voice channels to join them.",
 			github: "https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/DoubleClickVoiceChannels.plugin.js",
 			github_raw: "https://raw.githubusercontent.com/Metalloriff/BetterDiscordPlugins/master/DoubleClickVoiceChannels.plugin.js"
-		}
+		},
+		changelog:
+		[
+			{
+				title: "Patched",
+				type: "fixed",
+				items: ["Discord b0rked, I fixed."]
+			}
+		]
 	};
 
 	return !global.ZeresPluginLibrary ? class
@@ -60,7 +68,7 @@ module.exports = (() =>
 
 		const plugin = (Plugin, Api) =>
 		{
-			const { WebpackModules, Patcher } = Api;
+			const { WebpackModules, Patcher, Utilities } = Api;
 
 			const ChannelItem = WebpackModules.getByDisplayName("ChannelItem");
 
@@ -69,6 +77,12 @@ module.exports = (() =>
 				constructor()
 				{
 					super();
+				}
+
+				async showChangelog(footer)
+				{
+					try { footer = (await WebpackModules.getByProps("getUser", "acceptAgreements").getUser("264163473179672576")).tag + " | https://discord.gg/yNqzuJa"; }
+					finally { super.showChangelog(footer); }
 				}
 	
 				onStart()
@@ -79,9 +93,18 @@ module.exports = (() =>
 						{
 							if (r.props.channel.type == 2)
 							{
-								const onClick = el.props.onClick;
-								el.props.onDoubleClick = onClick;
-								el.props.onClick = () => {};
+								const props = Utilities.getNestedProp(el, "props.children.1.props.children.0.props");
+
+								if (props)
+								{
+									const onClick = props.onClick;
+									props.onDoubleClick = onClick;
+									props.onClick = () => {};
+								}
+								else
+								{
+									console.warn("DoubleClickVoiceChannel: Failed to get nested props!");
+								}
 							}
 						});
 					}

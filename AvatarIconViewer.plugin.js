f8eacdb18b1d0f6654bcda0be0b4bb59271c0514
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2022-01-11 14:51:00 -0500

    Please someone fix this for me
    This is a horrible fix, but I can't be bothered to spend 25 hours figuring out what the Discord monkeys broke right now.

--- AvatarIconViewer.plugin.js
+++ AvatarIconViewer.plugin.js
@@ -1,52 +1,47 @@
 /**
  * @name AvatarIconViewer
  * @invite yNqzuJa
- * @authorLink https://github.com/Metalloriff
+ * @authorLink https://discord.com/users/264163473179672576
  * @donate https://www.paypal.me/israelboone
- * @website https://metalloriff.github.io/toms-discord-stuff/
+ * @website https://kinzoku.one/
  * @source https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/AvatarIconViewer.plugin.js
  */
 
-module.exports = (() =>
-{
+module.exports = (() => {
 	const config =
 	{
 		info:
 		{
 			name: "AvatarIconViewer",
 			authors:
-			[
-				{
-					name: "Metalloriff",
-					discord_id: "264163473179672576",
-					github_username: "metalloriff",
-					twitter_username: "Metalloriff"
-				}
-			],
-			version: "2.0.0",
+				[
+					{
+						name: "Metalloriff",
+						discord_id: "264163473179672576",
+						github_username: "metalloriff",
+						twitter_username: "Metalloriff"
+					}
+				],
+			version: "2.0.1",
 			description: "Allows you to view server icons, user avatars, and emotes in fullscreen via the context menu, or copy the link to them.",
 			github: "https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/AvatarIconViewer.plugin.js",
 			github_raw: "https://raw.githubusercontent.com/Metalloriff/BetterDiscordPlugins/master/AvatarIconViewer.plugin.js"
 		}
 	};
 
-	return !global.ZeresPluginLibrary ? class
-	{
+	return !global.ZeresPluginLibrary ? class {
 		constructor() { this._config = config; }
 
 		getName = () => config.info.name;
 		getAuthor = () => config.info.description;
 		getVersion = () => config.info.version;
 
-		load()
-		{
+		load() {
 			BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
 				confirmText: "Download Now",
 				cancelText: "Cancel",
-				onConfirm: () =>
-				{
-					require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (err, res, body) =>
-					{
+				onConfirm: () => {
+					require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (err, res, body) => {
 						if (err) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
 						await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
 					});
@@ -58,8 +53,7 @@ module.exports = (() =>
 		stop() { }
 	} : (([Plugin, Api]) => {
 
-		const plugin = (Plugin, Api) =>
-		{
+		const plugin = (Plugin, Api) => {
 			const { DiscordModules, WebpackModules, Patcher } = Api;
 			const { React, ModalStack } = DiscordModules
 
@@ -77,23 +71,38 @@ module.exports = (() =>
 						? url.replace(".webp", ".gif").replace(".png", ".gif")
 						: url).split("?")[0] + "?size=2048";
 
-			return class AvatarIconViewer extends Plugin
-			{
-				constructor()
-				{
+			return class AvatarIconViewer extends Plugin {
+				constructor() {
 					super();
 				}
-	
-				onStart()
-				{
-					const modules = WebpackModules.findAll(m => m.default && m.default.displayName && (m.default.displayName.endsWith("UserContextMenu") || m.default.displayName == "GroupDMContextMenu"));
+
+				async onStart() {
+					// I have no idea why, nor do I have the energy to figure out why,
+					// but shitcord does not load the context menu modules until the context menu is opened.
+
+					// Therefore, this shitty workaround was made, which requires the user to
+					// open the context menu once before the plugin will work.
+
+					let modules;
+					await new Promise(async resolve => {
+						// This is really, really bad. I'm sorry.
+						// Don't do this. Don't look at this.
+						// I'll fix it later™️
+
+						while (true) {
+							modules = WebpackModules.findAll(m => m.default && m.default.displayName && (m.default.displayName.endsWith("UserContextMenu") || m.default.displayName == "GroupDMContextMenu"));
+
+							await new Promise(r => setTimeout(r, 1000));
+
+							if (modules) resolve();
+						}
+					});
+
 					const GuildContextMenu = WebpackModules.find(m => m.default && m.default.displayName == "GuildContextMenu");
 					const MessageContextMenu = WebpackModules.find(m => m.default && m.default.displayName == "MessageContextMenu");
 
-					for (const m of modules)
-					{
-						Patcher.after(m, "default", (_, [props], re) =>
-						{
+					for (const m of modules) {
+						Patcher.after(m, "default", (_, [props], re) => {
 							const type = props.user ? "Avatar" : props.channel && props.channel.type == 3 ? "Icon" : null;
 							const url = formatURL(type == "Avatar" ? props.user.getAvatarURL() : type == "Icon" ? getChannelIconURL(props.channel) : null);
 
@@ -102,63 +111,60 @@ module.exports = (() =>
 						});
 					}
 
-					Patcher.after(GuildContextMenu, "default", (_, [props], re) =>
-					{
+					Patcher.after(GuildContextMenu, "default", (_, [props], re) => {
 						const url = formatURL(props.guild.getIconURL());
 
 						if (url)
 							re.props.children.push(this.createContext(url, "Icon"));
 					});
 
-					Patcher.after(MessageContextMenu, "default", (_, [props], re) =>
-					{
-						if (props.target && props.target.src)
-						{
+					Patcher.after(MessageContextMenu, "default", (_, [props], re) => {
+						if (props.target && props.target.src) {
 							re.props.children.push(this.createContext(props.target.src, "Emoji"));
 						}
 					});
 				}
 
-				createContext(url, type)
-				{
+				createContext(url, type) {
 					return React.createElement(ContextMenu.MenuGroup,
-					{
-						children:
-						[
-							React.createElement(
-								ContextMenu.MenuItem,
-								{
-									label: "View " + type,
-									id: "aiv-view",
-									action: () =>
-										ModalStack.push(
-											ImageModal,
-											{
-												src: url,
-												placeholder: url,
-												original: url,
-												width: 2048,
-												height: 2048,
-												onClickUntrusted: e => e.openHref(),
-												renderLinkComponent: props => React.createElement(MaskedLink, props)
-											}
-										)
-								}
-							),
-							React.createElement(
-								ContextMenu.MenuItem,
-								{
-									label: "Copy " + type + " Link",
-									id: "aiv-copy",
-									action: () => copyToClipboard(url)
-								}
-							)
-						]
-					});
+						{
+							children:
+								[
+									React.createElement(
+										ContextMenu.MenuItem,
+										{
+											label: "View " + type,
+											id: "aiv-view",
+											action: () =>
+												window.open(url)
+											// TODO fix this at some point
+											// ModalStack.push(
+											// 	ImageModal,
+											// 	{
+											// 		src: url,
+											// 		placeholder: url,
+											// 		original: url,
+											// 		width: 2048,
+											// 		height: 2048,
+											// 		onClickUntrusted: e => e.openHref(),
+											// 		renderLinkComponent: props => React.createElement(MaskedLink, props)
+											// 	}
+											// )
+										}
+									),
+									React.createElement(
+										ContextMenu.MenuItem,
+										{
+											label: "Copy " + type + " Link",
+											id: "aiv-copy",
+											action: () => copyToClipboard(url)
+										}
+									)
+								]
+						});
 				}
-	
-				onStop()
-				{
+
+				onStop() {
 					Patcher.unpatchAll();
 				}
 			}
